a77d157791d20410c77d77c3c2b390f6a6a04f50
Adrian Sampson <adrian@radbox.org>
2019-04-02 15:24:07 -0400

    Try writing new general Makefile

---
+++ _common/sds.mk
@@ -0,0 +1,34 @@
+KERNEL:=stencil_3d
+HW_SRCS:=$(KERNEL).cpp
+HOST_SRCS:=main.cpp local_support.cpp common/support.cpp
+
+# A common Makefile for building FPGA+host code with SDSoC. Specify $(KERNEL),
+# $(HW_SRCS), and $(HOST_SRCS), and then `include` this file.
+
+SOURCES := $(HW_SRCS) $(HOST_SRCS)
+
+OBJECTS := $(SOURCES:%.cpp=%.o)
+DEPENDS := $(SOURCES:%.cpp=%.d)
+
+SDSXX := sds++ -sds-hw $(KERNEL) $(HW_SRCS) -sds-end
+SDSFLAGS := -sds-pf zed -clkid 3 -poll-mode 1 -verbose
+CXX := g++
+CXXFLAGS := -Wall -O3
+
+$(KERNEL): $(OBJECTS)
+	$(SDSXX) $(SDSFLAGS) $(CXXFLAGS) $(LDFLAGS) $^ -o $@
+
+%.o: %.cpp
+	$(SDSXX) $(SDSFLAGS) $(CXXFLAGS) -c $< -o $@
+
+.PHONY: clean
+clean:
+	rm -rf $(OBJECTS) $(DEPENDS) $(KERNEL)
+
+# Use the compiler's -MM flag to generate header dependencies. (sds++ seems to
+# not work correctly for this.)
+%.d: %.cpp
+	$(CXX) $(CXXFLAGS) -MM $^ > $@
+
+# Include the generated dependencies.
+include $(DEPENDS)
