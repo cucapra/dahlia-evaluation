0fbb4a9c124416cc656c61f0b6b5c5c8c0982e44
sa2257 <31310493+sa2257@users.noreply.github.com>
2019-07-12 10:01:11 -0400

    NW software debug (#70)
    * Debug nw
    
    * Update nw rewrite to match debugged code
    
    * Put ASCII values for chars

--- rewrite/machsuite-nw/nw.cpp
+++ rewrite/machsuite-nw/nw.cpp
@@ -9,6 +9,14 @@ using ap_int = int;
 #pragma SDS data zero_copy(M[0:16641], ptr[0:16641])  
 void nw(char SEQA[128], char SEQB[128], char alignedA[256], char alignedB[256], ap_int<32> M[16641], char ptr[16641]) {
   
+  ap_int<1> MATCH_SCORE = 1;
+  ap_int<32> MISMATCH_SCORE = -1;
+  ap_int<32> GAP_SCORE = -1;
+  ap_int<8> ALIGN = 92;
+  ap_int<8> SKIPA = 94;
+  ap_int<7> SKIPB = 60;
+  ap_int<7> DASH = 45;
+  ap_int<8> UNDERSCORE = 95;
   ap_int<32> score = 0;
   ap_int<32> up_left = 0;
   ap_int<32> up = 0;
@@ -17,16 +25,6 @@ void nw(char SEQA[128], char SEQB[128], char alignedA[256], char alignedB[256], 
   ap_int<32> row = 0;
   ap_int<32> row_up = 0;
   ap_int<32> r = 0;
-  ap_int<32> a_str_idx = 0;
-  ap_int<32> b_str_idx = 0;
-  ap_int<1> GAP_SCORE = 1;
-  ap_int<1> MATCH_SCORE = 1;
-  ap_int<1> MISMATCH_SCORE = 1;
-  ap_int<4> SKIPB = 5;
-  ap_int<4> SKIPA = 5;
-  ap_int<4> ALIGN = 6;
-  ap_int<4> DASH = 7;
-  ap_int<5> UNDERSCORE = 10;
   for(int a_idx = 0; a_idx < 129; a_idx++) {
 #pragma HLS loop_tripcount max=100 min=0
     M[a_idx] = (a_idx * GAP_SCORE);
@@ -34,7 +32,7 @@ void nw(char SEQA[128], char SEQB[128], char alignedA[256], char alignedB[256], 
   //---
   for(int b_idx = 0; b_idx < 129; b_idx++) {
 #pragma HLS loop_tripcount max=100 min=0
-    M[(b_idx * 129)] = b_idx;
+    M[(b_idx * 129)] = (b_idx * GAP_SCORE);
   }
   //---
   for(int b_idx = 1; b_idx < 129; b_idx++) {
@@ -44,13 +42,7 @@ void nw(char SEQA[128], char SEQB[128], char alignedA[256], char alignedB[256], 
       if((SEQA[(a_idx - 1)] == SEQB[(b_idx - 1)])){
         score = MATCH_SCORE;
       } else{
-        
-      }
-      //---
-      if((SEQA[(a_idx - 1)] != SEQB[(b_idx - 1)])){
         score = MISMATCH_SCORE;
-      } else{
-        
       }
       row_up = ((b_idx - 1) * 129);
       row = (b_idx * 129);
@@ -59,78 +51,64 @@ void nw(char SEQA[128], char SEQB[128], char alignedA[256], char alignedB[256], 
       up = (M[(row_up + a_idx)] + GAP_SCORE);
       //---
       left = (M[(row + (a_idx - 1))] + GAP_SCORE);
-      max = left;
-      if((up >= left)){
-        max = up;
-      } else{
-        
-      }
       if(((up_left >= up) && (up_left >= left))){
         max = up_left;
       } else{
-        
+          if((up >= left)){
+            max = up;
+          } else{
+            max = left;
+          }
       }
+      //---
+      M[row + a_idx] = max;
       if((max == left)){
         ptr[(row + a_idx)] = SKIPB;
       } else{
-        
-      }
-      //---
-      if((max == up)){
-        ptr[(row + a_idx)] = SKIPA;
-      } else{
-        
-      }
-      //---
-      if(((max != left) && (max != up))){
-        ptr[(row + a_idx)] = ALIGN;
-      } else{
-        
+        if((max == up)){
+          ptr[(row + a_idx)] = SKIPA;
+        } else{
+          ptr[(row + a_idx)] = ALIGN;
+        }
       }
     }
   }
   //---
-  ap_int<5> a_idx = 10;
+  ap_int<8> a_idx = 128;
   ap_int<8> b_idx = 128;
+  ap_int<32> a_str_idx = 0;
+  ap_int<32> b_str_idx = 0;
   while(((a_idx > 0) || (b_idx > 0))) {
 #pragma HLS loop_tripcount max=100 min=0
     r = (b_idx * 129);
-    a_str_idx = (a_str_idx + 1);
-    b_str_idx = (b_str_idx + 1);
     if((ptr[(r + a_idx)] == ALIGN)){
       alignedA[a_str_idx] = SEQA[(a_idx - 1)];
       alignedB[b_str_idx] = SEQB[(b_idx - 1)];
       a_idx = (a_idx - 1);
       b_idx = (b_idx - 1);
     } else{
-      
+      if((ptr[(r + a_idx)] == SKIPB)){
+        alignedA[a_str_idx] = SEQA[(a_idx - 1)];
+        alignedB[b_str_idx] = DASH;
+        a_idx = (a_idx - 1);
+      } else{
+        alignedA[a_str_idx] = DASH;
+        alignedB[b_str_idx] = SEQB[(b_idx - 1)];
+        b_idx = (b_idx - 1);
+      }
     }
+    a_str_idx = (a_str_idx + 1);
+    b_str_idx = (b_str_idx + 1);
+  }
     //---
-    if((ptr[(r + a_idx)] == SKIPB)){
-      alignedA[a_str_idx] = SEQA[(a_idx - 1)];
-      alignedB[b_str_idx] = DASH;
-      a_idx = (a_idx - 1);
-    } else{
-      
-    }
-    //---
-    if((ptr[(r + a_idx)] == SKIPA)){
-      alignedA[a_str_idx] = DASH;
-      alignedB[b_str_idx] = SEQB[(b_idx - 1)];
-      b_idx = (b_idx - 1);
-    } else{
-      
-    }
-    //---
-    while((a_str_idx < 138)) {
+    while((a_str_idx < 256)) {
 #pragma HLS loop_tripcount max=100 min=0
       alignedA[a_str_idx] = UNDERSCORE;
       a_str_idx = (a_str_idx + 1);
     }
-    while((a_str_idx < 138)) {
+    while((b_str_idx < 256)) {
 #pragma HLS loop_tripcount max=100 min=0
       alignedB[b_str_idx] = UNDERSCORE;
       b_str_idx = (b_str_idx + 1);
     }
-  }
 }
