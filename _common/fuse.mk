c0bb97f161f968afccf51dffb3b586f806a4fba0
Adrian Sampson <adrian@radbox.org>
2019-07-15 09:40:12 -0400

    Conditionally set FUSE in fuse.mk

--- _common/fuse.mk
+++ _common/fuse.mk
@@ -1,6 +1,6 @@
 # By default, we look for the Fuse compiler in a directory adjacent to the
 # benchmarks directory.
-FUSE := ../../../seashell/target/scala-2.12/fuse.jar
+FUSE ?= ../../../seashell/target/scala-2.12/fuse.jar
 
 # The `make fuse` target rebuilds the HLS C++ program from the Fuse source.
 # For now, it assumes that HW_SRCS consists of a single *.cpp file.
