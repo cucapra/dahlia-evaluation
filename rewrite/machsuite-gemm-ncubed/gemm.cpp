e7f97072acc90824f21060b87a013c5e9c0ebea3
sa2257 <31310493+sa2257@users.noreply.github.com>
2019-07-19 09:54:47 -0400

    GEMM-NCUBED rewrite with our language (#81)
    * Update gemm rewrite to look more seashell-like
    
    * Add data copy pragma
    
    * Attemp pointer casting
    
    * Possible changes in software for multi-d arrays
    
    * Add new files to separate rewrite and baseline execution with differing interfaces
    
    * Remove source file include

--- rewrite/machsuite-gemm-ncubed/gemm.cpp
+++ rewrite/machsuite-gemm-ncubed/gemm.cpp
@@ -7,25 +7,21 @@ template <int N> using ap_uint = unsigned int;
 #endif
 
 
-
+#pragma SDS data copy(m1[0:row_size][0:col_size])
 void gemm(
-double m1[4096], 
-double m2[4096], 
-double prod[4096]) {
+double m1[64][64], 
+double m2[64][64], 
+double prod[64][64]) {
   
-  ap_int<32> i_col = 0;
-  ap_int<32> k_col = 0;
-  double mult = 0.0;
   for(int i = 0; i < 64; i++) {
     for(int j = 0; j < 64; j++) {
-      i_col = (i * 64);
       double sum = 0.0;
       for(int k = 0; k < 64; k++) {
-        k_col = (k * 64);
-        mult = (m1[(i_col + k)] * m2[(k_col + j)]);
-        sum = (sum + mult);
+        double mult = (m1[i][k] * m2[k][j]);
+        // combiner:
+        sum += mult;
       }
-      prod[(i_col + j)] = sum;
+      prod[i][j] = sum;
     }
   }
 }
