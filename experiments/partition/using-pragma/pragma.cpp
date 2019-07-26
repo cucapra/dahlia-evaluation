39c54e8671e08ac873d3ffb158834c1ab9b9c7ff
Adrian Sampson <adrian@radbox.org>
2019-07-26 19:00:43 -0400

    Partitioning experiment: use global arrays
    As discussed in #93.

--- experiments/partition/using-pragma/pragma.cpp
+++ experiments/partition/using-pragma/pragma.cpp
@@ -1,20 +1,17 @@
 // Avoid using `ap_int` in "software" compilation.
 #include "pragma.h"
 
+ap_int<32> A[32][32];
+#pragma HLS ARRAY_PARTITION variable=A factor=2 dim=1
+#pragma HLS ARRAY_PARTITION variable=A factor=2 dim=2
+
+ap_int<32> B[32][32];
+#pragma HLS ARRAY_PARTITION variable=B factor=2 dim=1
+#pragma HLS ARRAY_PARTITION variable=B factor=2 dim=2
+
+ap_int<32> res;
 
 void kernel(ap_int<32> iterations) {
-
-
-  #pragma HLS ARRAY_PARTITION variable=A factor=2 dim=1
-  #pragma HLS ARRAY_PARTITION variable=A factor=2 dim=2
-  ap_int<32> A[32][32];
-
-  #pragma HLS ARRAY_PARTITION variable=B factor=2 dim=1
-  #pragma HLS ARRAY_PARTITION variable=B factor=2 dim=2
-  ap_int<32> B[32][32];
-  ap_int<32> res;
-
-
   while((iterations > 0)) {
     res = 0;
     for(int i = 0; i < 32; i++) {
