6456bac784e43a8bf2d7309c3ca444c6662cbaaa
Adrian Sampson <adrian@radbox.org>
2019-07-30 21:40:13 -0400

    Add fake_ap_int header
    A trick to support ap_int in software execution.

---
+++ _common/fake_ap_int.h
@@ -0,0 +1,55 @@
+#ifdef __SDSCC__
+
+// In "hardware" mode (i.e., SDSoC compilation), use the real/provided ap_int
+// library.
+
+#include <ap_int.h>
+
+#else
+
+// In "software" mode (i.e., a normal C++ compiler), use standard int types.
+// Thanks to Matthew Milano for his help figuring out how to do this.
+
+#include <stdint.h>
+
+// ap_int
+
+template <int N>
+struct _int_hack { static_assert(N != N, "precision unavailable"); };
+
+template <>
+struct _int_hack<8> { using t = int8_t; };
+
+template <>
+struct _int_hack<16> { using t = int16_t; };
+
+template <>
+struct _int_hack<32> { using t = int32_t; };
+
+template <>
+struct _int_hack<64> { using t = int64_t; };
+
+template <int N>
+using ap_int = typename _int_hack<N>::t;
+
+// ap_uint
+
+template <int N>
+struct _uint_hack { static_assert(N != N, "precision unavailable"); };
+
+template <>
+struct _uint_hack<8> { using t = uint8_t; };
+
+template <>
+struct _uint_hack<16> { using t = uint16_t; };
+
+template <>
+struct _uint_hack<32> { using t = uint32_t; };
+
+template <>
+struct _uint_hack<64> { using t = uint64_t; };
+
+template <int N>
+using ap_uint = typename _uint_hack<N>::t;
+
+#endif
