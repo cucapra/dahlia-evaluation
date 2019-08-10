c7cfce0bdaac09190dea898d2aaf4a9604c3f6fd
Vivi Ye <viviyyw0418@gmail.com>
2019-08-10 00:53:53 -0400

    working ncube

--- baseline/machsuite-gemm-ncubed/gemm.cpp
+++ baseline/machsuite-gemm-ncubed/gemm.cpp
@@ -1,19 +1,25 @@
-#include "func.h"
-
+#include "gemm.h"
+extern "C" {
 void gemm( TYPE m1[N], TYPE m2[N], TYPE prod[N] ){
-#pragma HLS INTERFACE s_axilite port=m1
-#pragma HLS INTERFACE s_axilite port=m2
-#pragma HLS INTERFACE s_axilite port=prod
-
+#pragma HLS INTERFACE m_axi port = m1 offset = slave bundle = gmem
+#pragma HLS INTERFACE m_axi port = m2 offset = slave bundle = gmem
+#pragma HLS INTERFACE m_axi port = prod offset = slave bundle = gmem
+#pragma HLS INTERFACE s_axilite port = m1 bundle = control
+#pragma HLS INTERFACE s_axilite port = m2 bundle = control
+#pragma HLS INTERFACE s_axilite port = prod bundle = control
+#pragma HLS INTERFACE s_axilite port = return bundle = control
     int i, j, k;
     int k_col, i_col;
     TYPE mult;
 
     outer:for(i=0;i<row_size;i++) {
+        #pragma HLS LOOP_TRIPCOUNT min=1 max=64
         middle:for(j=0;j<col_size;j++) {
+             #pragma HLS LOOP_TRIPCOUNT min=1 max=64
             i_col = i * col_size;
             TYPE sum = 0;
             inner:for(k=0;k<row_size;k++) {
+                #pragma HLS LOOP_TRIPCOUNT min=1 max=64
                 k_col = k * col_size;
                 mult = m1[i_col + k] * m2[k_col + j];
                 sum += mult;
@@ -21,4 +27,5 @@ void gemm( TYPE m1[N], TYPE m2[N], TYPE prod[N] ){
             prod[i_col + j]  = sum;
         }
     }
+}
 }
--- baseline/machsuite-gemm-ncubed/local_support.cpp
+++ baseline/machsuite-gemm-ncubed/local_support.cpp
@@ -1,16 +1,100 @@
-#include "gemm.h"
 #include <string.h>
-
-int INPUT_SIZE = sizeof(struct bench_args_t);
+#include "xcl2.hpp"
+#include <vector>
+#include "gemm.h"
 
 #define EPSILON ((TYPE)1.0e-6)
 
+int INPUT_SIZE = sizeof(struct bench_args_t);
+
+void run_benchmark( void *vargs ) {
+  struct bench_args_t *args = (struct bench_args_t *)vargs;
+  size_t vector_size_bytes = N*sizeof(TYPE);
+  cl_int err;
+  std::vector<TYPE,aligned_allocator<TYPE>> m_in1(4096);
+  std::vector<TYPE,aligned_allocator<TYPE>> m_in2(4096);
+  std::vector<TYPE,aligned_allocator<TYPE>> m_prod(4096);
+  
+  // Copy the test data 
+  for(int i = 0 ; i < 4096 ; i++){
+      m_in1[i] = args->m1[i];
+      m_in2[i] = args->m2[i];
+  }
+
+  // OPENCL HOST CODE AREA START
+    // get_xil_devices() is a utility API which will find the xilinx
+    // platforms and will return list of devices connected to Xilinx platform
+    std::vector<cl::Device> devices = xcl::get_xil_devices();
+    cl::Device device = devices[0];
+
+    OCL_CHECK(err, cl::Context context(device, NULL, NULL, NULL, &err));
+    OCL_CHECK(err, cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
+    OCL_CHECK(err, std::string device_name = device.getInfo<CL_DEVICE_NAME>(&err)); 
+
+    // find_binary_file() is a utility API which will search the xclbin file for
+    // targeted mode (sw_emu/hw_emu/hw) and for targeted platforms.
+    std::string binaryFile = xcl::find_binary_file(device_name,"gemm");
+
+    // import_binary_file() ia a utility API which will load the binaryFile
+    // and will return Binaries.
+    cl::Program::Binaries bins = xcl::import_binary_file(binaryFile);
+    devices.resize(1);
+    OCL_CHECK(err, cl::Program program(context, devices, bins, NULL, &err));
+    OCL_CHECK(err, cl::Kernel krnl_gemm_ncube(program,"gemm", &err));
+
+    // Allocate Buffer in Global Memory
+    // Buffers are allocated using CL_MEM_USE_HOST_PTR for efficient memory and 
+    // Device-to-host communication
+    OCL_CHECK(err,
+              cl::Buffer buffer_in1(context,
+                                    CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                                    vector_size_bytes,
+                                    m_in1.data(),
+                                    &err));
+    OCL_CHECK(err,
+              cl::Buffer buffer_in2(context,
+                                    CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                                    vector_size_bytes,
+                                    m_in2.data(),
+                                    &err));
+    OCL_CHECK(err,
+              cl::Buffer buffer_output(context,
+                                       CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+                                       vector_size_bytes,
+                                       m_prod.data(),
+                                       &err));
+
+
+    // Copy input data to device global memory
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_in1, buffer_in2},0/* 0 means from host*/));
+
+    int size = 4096;
+    OCL_CHECK(err, err = krnl_gemm_ncube.setArg(0, buffer_in1));
+    OCL_CHECK(err, err = krnl_gemm_ncube.setArg(1, buffer_in2));
+    OCL_CHECK(err, err = krnl_gemm_ncube.setArg(2, buffer_output));
+
+
+    // Launch the Kernel
+    // For HLS kernels global and local size is always (1,1,1). So, it is recommended
+    // to always use enqueueTask() for invoking HLS kernel
+    OCL_CHECK(err, err = q.enqueueTask(krnl_gemm_ncube));
+
+    // Copy Result from Device Global Memory to Host Local Memory
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output},CL_MIGRATE_MEM_OBJECT_HOST));
+    q.finish();
+  // OPENCL HOST CODE AREA END
+  
+  // Copy results 
+  for(int i = 0 ; i < 4096  ; i++){
+      args->prod[i] = m_prod[i];
+  }
+}
 
 /* Input format:
 %% Section 1
-TYPE[N]: matrix 1
+TYPE[4096 ]: matrix 1
 %% Section 2
-TYPE[N]: matrix 2
+TYPE[4096 ]: matrix 2
 */
 
 void input_to_data(int fd, void *vdata) {
@@ -22,10 +106,10 @@ void input_to_data(int fd, void *vdata) {
   p = readfile(fd);
 
   s = find_section_start(p,1);
-  STAC(parse_,TYPE,_array)(s, data->m1, N);
+  STAC(parse_,TYPE,_array)(s, data->m1, 4096);
 
   s = find_section_start(p,2);
-  STAC(parse_,TYPE,_array)(s, data->m2, N);
+  STAC(parse_,TYPE,_array)(s, data->m2, 4096);
   free(p);
 
 }
@@ -34,15 +118,15 @@ void data_to_input(int fd, void *vdata) {
   struct bench_args_t *data = (struct bench_args_t *)vdata;
 
   write_section_header(fd);
-  STAC(write_,TYPE,_array)(fd, data->m1, N);
+  STAC(write_,TYPE,_array)(fd, data->m1, 4096);
 
   write_section_header(fd);
-  STAC(write_,TYPE,_array)(fd, data->m2, N);
+  STAC(write_,TYPE,_array)(fd, data->m2, 4096);
 }
 
 /* Output format:
 %% Section 1
-TYPE[N]: output matrix
+TYPE[4096]: output matrix
 */
 
 void output_to_data(int fd, void *vdata) {
@@ -52,7 +136,7 @@ void output_to_data(int fd, void *vdata) {
   p = readfile(fd);
 
   s = find_section_start(p,1);
-  STAC(parse_,TYPE,_array)(s, data->prod, N);
+  STAC(parse_,TYPE,_array)(s, data->prod, 4096);
   free(p);
 }
 
@@ -60,7 +144,7 @@ void data_to_output(int fd, void *vdata) {
   struct bench_args_t *data = (struct bench_args_t *)vdata;
 
   write_section_header(fd);
-  STAC(write_,TYPE,_array)(fd, data->prod, N);
+  STAC(write_,TYPE,_array)(fd, data->prod, 4096);
 }
 
 int check_data( void *vdata, void *vref ) {
