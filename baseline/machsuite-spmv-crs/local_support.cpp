cb2f104d3cda13f7fa2b24efb959116b94d552f0
Vivi Ye <viviyyw0418@gmail.com>
2019-08-12 02:40:28 -0400

    spmv working with sdaccel

--- baseline/machsuite-spmv-crs/local_support.cpp
+++ baseline/machsuite-spmv-crs/local_support.cpp
@@ -1,3 +1,5 @@
+#include "xcl2.hpp"
+#include <vector>
 #include "spmv.h"
 #include <string.h>
 
@@ -7,8 +9,116 @@ int INPUT_SIZE = sizeof(struct bench_args_t);
 
 void run_benchmark( void *vargs ) {
   struct bench_args_t *args = (struct bench_args_t *)vargs;
-  spmv( args->val, args->cols, args->rowDelimiters, args->vec, args->out );
+  size_t vector_size_bytes_val = NNZ*sizeof(TYPE);
+  size_t vector_size_bytes_cols = NNZ*sizeof(int32_t);
+  size_t vector_size_bytes_rowDelimiters = (N+1)*sizeof(int32_t);
+  size_t vector_size_bytes_vec = N*sizeof(TYPE);
+  size_t vector_size_bytes_out = N*sizeof(TYPE);
+
+  cl_int err;
+  std::vector<TYPE,aligned_allocator<TYPE>> val_in(NNZ);
+  std::vector<int32_t,aligned_allocator<int32_t>> cols_in(NNZ);
+  std::vector<int32_t,aligned_allocator<int32_t>> rowDelimiters_in(N+1);
+  std::vector<TYPE,aligned_allocator<TYPE>> vec_in(N);
+  std::vector<TYPE,aligned_allocator<TYPE>> out(N);
+  
+  // Copy the test data 
+  for(int i = 0 ; i < NNZ ; i++){
+      val_in[i] = args->val[i];
+      cols_in[i] = args->cols[i];
+      if (i < N) {
+      vec_in[i] = args->vec[i];
+      out[i] = args->out[i];
+      }
+      if (i < N+1){
+      rowDelimiters_in[i] = args->rowDelimiters[i];
+      }
+  }
+
+  // OPENCL HOST CODE AREA START
+    // get_xil_devices() is a utility API which will find the xilinx
+    // platforms and will return list of devices connected to Xilinx platform
+    std::vector<cl::Device> devices = xcl::get_xil_devices();
+    cl::Device device = devices[0];
+
+    OCL_CHECK(err, cl::Context context(device, NULL, NULL, NULL, &err));
+    OCL_CHECK(err, cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
+    OCL_CHECK(err, std::string device_name = device.getInfo<CL_DEVICE_NAME>(&err)); 
+
+    // find_binary_file() is a utility API which will search the xclbin file for
+    // targeted mode (sw_emu/hw_emu/hw) and for targeted platforms.
+    std::string binaryFile = xcl::find_binary_file(device_name,"spmv");
+
+    // import_binary_file() ia a utility API which will load the binaryFile
+    // and will return Binaries.
+    cl::Program::Binaries bins = xcl::import_binary_file(binaryFile);
+    devices.resize(1);
+    OCL_CHECK(err, cl::Program program(context, devices, bins, NULL, &err));
+    OCL_CHECK(err, cl::Kernel krnl_spmv_crs(program,"spmv", &err));
+
+    // Allocate Buffer in Global Memory
+    // Buffers are allocated using CL_MEM_USE_HOST_PTR for efficient memory and 
+    // Device-to-host communication
+    OCL_CHECK(err,
+              cl::Buffer buffer_val(context,
+                                    CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                                    vector_size_bytes_val,
+                                    val_in.data(),
+                                    &err));
+    OCL_CHECK(err,
+              cl::Buffer buffer_cols(context,
+                                    CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                                    vector_size_bytes_cols,
+                                    cols_in.data(),
+                                    &err));
+    OCL_CHECK(err,
+              cl::Buffer buffer_rowDelimiters(context,
+                                    CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                                    vector_size_bytes_rowDelimiters,
+                                    rowDelimiters_in.data(),
+                                    &err));
+    OCL_CHECK(err,
+              cl::Buffer buffer_vec(context,
+                                    CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+                                    vector_size_bytes_vec,
+                                    vec_in.data(),
+                                    &err));
+    OCL_CHECK(err,
+              cl::Buffer buffer_output(context,
+                                       CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+                                       vector_size_bytes_out,
+                                       out.data(),
+                                       &err));
+
+
+    // Copy input data to device global memory
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_val, buffer_cols, buffer_rowDelimiters, buffer_vec, buffer_output},0/* 0 means from host*/));
+
+    int size = 4096;
+    OCL_CHECK(err, err = krnl_spmv_crs.setArg(0, buffer_val));
+    OCL_CHECK(err, err = krnl_spmv_crs.setArg(1, buffer_cols));
+    OCL_CHECK(err, err = krnl_spmv_crs.setArg(2, buffer_rowDelimiters));
+    OCL_CHECK(err, err = krnl_spmv_crs.setArg(3, buffer_vec));
+    OCL_CHECK(err, err = krnl_spmv_crs.setArg(4, buffer_output));
+
+
+    // Launch the Kernel
+    // For HLS kernels global and local size is always (1,1,1). So, it is recommended
+    // to always use enqueueTask() for invoking HLS kernel
+    OCL_CHECK(err, err = q.enqueueTask(krnl_spmv_crs));
+
+    // Copy Result from Device Global Memory to Host Local Memory
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output},CL_MIGRATE_MEM_OBJECT_HOST));
+    q.finish();
+  // OPENCL HOST CODE AREA END
+  
+  // Copy results 
+  for(int i = 0 ; i < N  ; i++){
+      args->out[i] = out[i];
+  }
 }
+
+
 
 /* Input format:
 %% Section 1
--- baseline/machsuite-spmv-crs/spmv.cpp
+++ baseline/machsuite-spmv-crs/spmv.cpp
@@ -5,26 +5,36 @@ http://www.cs.berkeley.edu/~mhoemmen/matrix-seminar/slides/UCB_sparse_tutorial_1
 
 #include "spmv.h"
 
+extern"C" {
 void spmv(TYPE val[NNZ], int32_t cols[NNZ], int32_t rowDelimiters[N+1], TYPE vec[N], TYPE out[N]){
-#pragma HLS INTERFACE s_axilite port=val
-#pragma HLS INTERFACE s_axilite port=cols
-#pragma HLS INTERFACE s_axilite port=rowDelimiters
-#pragma HLS INTERFACE s_axilite port=vec
-#pragma HLS INTERFACE s_axilite port=out
+#pragma HLS INTERFACE m_axi port = val offset = slave bundle = gmem
+#pragma HLS INTERFACE m_axi port = cols offset = slave bundle = gmem
+#pragma HLS INTERFACE m_axi port = rowDelimiters offset = slave bundle = gmem
+#pragma HLS INTERFACE m_axi port = vec offset = slave bundle = gmem
+#pragma HLS INTERFACE m_axi port = out offset = slave bundle = gmem
+#pragma HLS INTERFACE s_axilite port=val bundle = control
+#pragma HLS INTERFACE s_axilite port=cols bundle = control
+#pragma HLS INTERFACE s_axilite port=rowDelimiters bundle = control
+#pragma HLS INTERFACE s_axilite port=vec bundle = control
+#pragma HLS INTERFACE s_axilite port=out bundle = control
+#pragma HLS INTERFACE s_axilite port = return bundle = control
+
 
     int i, j;
     TYPE sum, Si;
 
     spmv_1 : for(i = 0; i < N; i++){
+        #pragma HLS LOOP_TRIPCOUNT min=1 max=494
         sum = 0; Si = 0;
         int tmp_begin = rowDelimiters[i];
         int tmp_end = rowDelimiters[i+1];
         spmv_2 : for (j = tmp_begin; j < tmp_end; j++){
+             #pragma HLS LOOP_TRIPCOUNT min=1 max=494
             Si = val[j] * vec[cols[j]];
             sum = sum + Si;
         }
         out[i] = sum;
     }
 }
-
+}
 
