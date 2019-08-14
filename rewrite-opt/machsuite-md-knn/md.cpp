69f81b0175431bcb4c84933c466c73ceeb50f074
Adrian Sampson <adrian@radbox.org>
2019-08-14 17:34:52 -0400

    Try optimizing md-knn??

--- rewrite-opt/machsuite-md-knn/md.cpp
+++ rewrite-opt/machsuite-md-knn/md.cpp
@@ -1,6 +1,12 @@
 #include "ap_int.h"
 
-
+double at_j(double pos[256], ap_int<32> j) {
+  
+  
+  #pragma HLS ARRAY_PARTITION variable=pos cyclic factor=8 dim=1
+  
+  return pos[j];
+}
 
 void md(double force_x[256], double force_y[256], double force_z[256], double position_x[256], double position_y[256], double position_z[256], ap_int<32> nl[256][16]) {
   #pragma HLS INTERFACE s_axilite port=force_x
@@ -11,11 +17,26 @@ void md(double force_x[256], double force_y[256], double force_z[256], double po
   #pragma HLS INTERFACE s_axilite port=position_z
   #pragma HLS INTERFACE s_axilite port=nl
   
+  #pragma HLS ARRAY_PARTITION variable=force_x cyclic factor=8 dim=1
+  
+  #pragma HLS ARRAY_PARTITION variable=force_y cyclic factor=8 dim=1
+  
+  #pragma HLS ARRAY_PARTITION variable=force_z cyclic factor=8 dim=1
+  
+  #pragma HLS ARRAY_PARTITION variable=position_x cyclic factor=8 dim=1
+  
+  #pragma HLS ARRAY_PARTITION variable=position_y cyclic factor=8 dim=1
+  
+  #pragma HLS ARRAY_PARTITION variable=position_z cyclic factor=8 dim=1
+  
+  #pragma HLS ARRAY_PARTITION variable=nl cyclic factor=8 dim=1
   double lj1 = 1.5;
   
   double lj2 = 2.0;
   
   for(int i = 0; i < 256; i++) {
+    #pragma HLS UNROLL factor=8 skip_exit_check
+    
     double ix = position_x[i];
     
     double iy = position_y[i];
@@ -32,11 +53,11 @@ void md(double force_x[256], double force_y[256], double force_z[256], double po
     for(int j = 0; j < 16; j++) {
       ap_int<32> j_idx = nl[i][j];
       
-      double jx = position_x[j_idx];
+      double jx = at_j(position_x, j);
       
-      double jy = position_y[j_idx];
+      double jy = at_j(position_y, j);
       
-      double jz = position_z[j_idx];
+      double jz = at_j(position_z, j);
       
       double delx = (ix - jx);
       
