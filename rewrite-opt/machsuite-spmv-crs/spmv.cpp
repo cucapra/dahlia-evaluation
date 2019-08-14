d0ed33462b2ec0bf91850972e6ead7a2317bf133
Adrian Sampson <adrian@radbox.org>
2019-08-14 18:15:39 -0400

    Try optimizing spmv-crs??

--- rewrite-opt/machsuite-spmv-crs/spmv.cpp
+++ rewrite-opt/machsuite-spmv-crs/spmv.cpp
@@ -2,13 +2,16 @@
 
 
 #pragma SDS data copy(val[0:NNZ])
-void spmv(double val[1666], ap_int<32> cols[1666], ap_int<32> row_delimiters[495], double vec[494], double out[494]) {
+void spmv(double val[1672], ap_int<32> cols[1672], ap_int<32> row_delimiters[496], double vec[496], double out[496]) {
   #pragma HLS INTERFACE s_axilite port=val
   #pragma HLS INTERFACE s_axilite port=cols
   #pragma HLS INTERFACE s_axilite port=row_delimiters
   #pragma HLS INTERFACE s_axilite port=vec
   #pragma HLS INTERFACE s_axilite port=out
   
+  #pragma HLS ARRAY_PARTITION variable=row_delimiters cyclic factor=8 dim=1
+  
+  #pragma HLS ARRAY_PARTITION variable=out cyclic factor=8 dim=1
   double sum = 0.0;
   
   double si = 0.0;
@@ -19,15 +22,18 @@ void spmv(double val[1666], ap_int<32> cols[1666], ap_int<32> row_delimiters[495
   
   ap_int<32> j = 0;
   
-  for(int i = 0; i < 494; i++) {
+  
+  for(int i = 0; i < 496; i++) {
+    #pragma HLS UNROLL factor=8 skip_exit_check
+    
     sum = 0.0;
     si = 0.0;
     tmp_begin = row_delimiters[i];
     //---
-    tmp_end = row_delimiters[(i + 1)];
+    tmp_end = row_delimiters[(1 + i)];
     j = tmp_begin;
     while((j < tmp_end)) {
-      #pragma HLS loop_tripcount max=494 min=0
+      #pragma HLS loop_tripcount max=496 min=0
       si = (val[j] * vec[cols[j]]);
       sum = (sum + si);
       j = (j + 1);
