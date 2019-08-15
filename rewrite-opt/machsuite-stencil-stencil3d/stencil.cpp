b24fb9429d2b8d4ac6a522e16d2995514f84c2a8
Sachille Atapattu <sa2257@cornell.edu>
2019-08-15 06:33:56 -0400

    Optimizie stencil3d with a loop change

--- rewrite-opt/machsuite-stencil-stencil3d/stencil.cpp
+++ rewrite-opt/machsuite-stencil-stencil3d/stencil.cpp
@@ -2,21 +2,16 @@
 
 
 #pragma SDS data copy(C[0:2])
-void stencil(ap_int<32> C[2], ap_int<32> orig[32][32][16], ap_int<32> sol[32][32][16]) {
+void stencil(ap_int<32> C[2], ap_int<32> orig[33][33][18], ap_int<32> sol[32][32][16]) {
   #pragma HLS INTERFACE s_axilite port=C
   #pragma HLS INTERFACE s_axilite port=orig
   #pragma HLS INTERFACE s_axilite port=sol
   
   #pragma HLS ARRAY_PARTITION variable=C cyclic factor=2 dim=1
   
-  #pragma HLS ARRAY_PARTITION variable=orig cyclic factor=2 dim=1
-  #pragma HLS ARRAY_PARTITION variable=orig cyclic factor=2 dim=2
-  #pragma HLS ARRAY_PARTITION variable=orig cyclic factor=2 dim=3
-  
-  #pragma HLS ARRAY_PARTITION variable=sol cyclic factor=2 dim=1
-  #pragma HLS ARRAY_PARTITION variable=sol cyclic factor=2 dim=2
-  #pragma HLS ARRAY_PARTITION variable=sol cyclic factor=2 dim=3
-  
+  #pragma HLS ARRAY_PARTITION variable=orig cyclic factor=3 dim=1
+  #pragma HLS ARRAY_PARTITION variable=orig cyclic factor=3 dim=2
+  #pragma HLS ARRAY_PARTITION variable=orig cyclic factor=3 dim=3
   
   for(int j = 0; j < 32; j++) {
     for(int k = 0; k < 16; k++) {
@@ -27,7 +22,6 @@ void stencil(ap_int<32> C[2], ap_int<32> orig[32][32][16], ap_int<32> sol[32][32
   }
   //---
   
-  
   for(int i = 1; i < 31; i++) {
     for(int k = 0; k < 16; k++) {
       sol[i][0][k] = orig[i][0][k];
@@ -37,7 +31,6 @@ void stencil(ap_int<32> C[2], ap_int<32> orig[32][32][16], ap_int<32> sol[32][32
   }
   //---
   
-  
   for(int i = 1; i < 31; i++) {
     for(int j = 1; j < 31; j++) {
       sol[i][j][0] = orig[i][j][0];
@@ -46,32 +39,38 @@ void stencil(ap_int<32> C[2], ap_int<32> orig[32][32][16], ap_int<32> sol[32][32
     }
   }
   //---
-  
-  for(int i = 1; i < 31; i++) {
-    for(int j = 1; j < 31; j++) {
-      for(int k = 1; k < 15; k++) {
+  for(int i = 0; i < 30; i++) {
+    for(int j = 0; j < 30; j++) {
+      #pragma HLS pipeline
+      for(int k = 0; k < 14; k++) {
+        #pragma HLS pipeline
+        
         
         ap_int<32> temp_1 = 0;
         
-        for(int l1 = 0; l1 < 2; l1++) {
-          #pragma HLS UNROLL factor=2 skip_exit_check
+        for(int l1 = 0; l1 < 3; l1++) {
+          #pragma HLS UNROLL factor=3 skip_exit_check
           
           ap_int<32> temp_2 = 0;
           
-          for(int l2 = 0; l2 < 2; l2++) {
-            #pragma HLS UNROLL factor=2 skip_exit_check
+          for(int l2 = 0; l2 < 3; l2++) {
+            #pragma HLS UNROLL factor=3 skip_exit_check
             
             ap_int<32> temp_3 = 0;
             
-            for(int l3 = 0; l3 < 2; l3++) {
-              #pragma HLS UNROLL factor=2 skip_exit_check
+            for(int l3 = 0; l3 < 3; l3++) {
+              #pragma HLS UNROLL factor=3 skip_exit_check
               
               ap_int<32> multiply = 0;
               
-              if(((l1 == 0) && ((l2 == 0) && (l3 == 0)))){
+              if(((l1 == 1) && ((l2 == 1) && (l3 == 1)))){
                 multiply = C[0];
               } else{
-                multiply = C[1];
+                if((((l1 == 1) && (l2 == 1)) || (((l2 == 1) && (l3 == 1)) || ((l3 == 1) && (l1 == 1))))){
+                  multiply = C[1];
+                } else{
+                  multiply = 0;
+                }
               }
               ap_int<32> mul = (orig[(i + l1)][(j + l2)][(k + l3)] * multiply);
               
@@ -84,7 +83,7 @@ void stencil(ap_int<32> C[2], ap_int<32> orig[32][32][16], ap_int<32> sol[32][32
           // combiner:
           temp_1 += temp_2;
         }
-        sol[i][j][k] = temp_1;
+        sol[(1 + i)][(1 + j)][(1 + k)] = temp_1;
       }
     }
   }
