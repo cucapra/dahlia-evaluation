b35e8a137bb9c46e221091651dd4323d342fd9f8
Sachille Atapattu <sa2257@cornell.edu>
2019-08-15 10:33:46 -0400

    Add trip count to sort-merge

--- baseline-no_opt/machsuite-sort-merge/sort.cpp
+++ baseline-no_opt/machsuite-sort-merge/sort.cpp
@@ -1,12 +1,22 @@
 #include "sort.h"
 
+  int max1 =0; int min1 = 0; int number1 = 0; int total1 = 0;
+  int max3 =0; int min3 = 0; int number3 = 0; int total3 = 0;
 void merge(TYPE a[SIZE], int start, int m, int stop){
     TYPE temp[SIZE];
     int i, j, k;
 
+        int count1 = 0;
+        number1 += 1;
     merge_label1 : for(i=start; i<=m; i++){
+          count1 += 1;
         temp[i] = a[i];
     }
+        total1 += count1;
+        if (count1 >= max1)
+            max1 = count1;
+        if (count1 <= min1)
+            min1 = count1;
 
     merge_label2 : for(j=m+1; j<=stop; j++){
         temp[m+1+stop-j] = a[j];
@@ -15,7 +25,10 @@ void merge(TYPE a[SIZE], int start, int m, int stop){
     i = start;
     j = stop;
 
+        int count3 = 0;
+        number3 += 1;
     merge_label3 : for(k=start; k<=stop; k++){
+          count3 += 1;
         TYPE tmp_j = temp[j];
         TYPE tmp_i = temp[i];
         if(tmp_j < tmp_i) {
@@ -26,6 +39,11 @@ void merge(TYPE a[SIZE], int start, int m, int stop){
             i++;
         }
     }
+        total3 += count3;
+        if (count3 >= max3)
+            max3 = count3;
+        if (count3 <= min3)
+            min3 = count3;
 }
 
 void sort(TYPE a[SIZE]) {
@@ -36,8 +54,12 @@ void sort(TYPE a[SIZE]) {
     start = 0;
     stop = SIZE;
 
+  int max =0; int min = 0; int number = 0; int total = 0;
+        int count = 0;
+        number += 1;
     mergesort_label1 : for(m=1; m<stop-start; m+=m) {
         mergesort_label2 : for(i=start; i<stop; i+=m+m) {
+          count += 1;
             from = i;
             mid = i+m-1;
             to = i+m+m-1;
@@ -49,4 +71,12 @@ void sort(TYPE a[SIZE]) {
             }
         }
     }
+        total += count;
+        if (count >= max)
+            max = count;
+        if (count <= min)
+            min = count;
+        printf("1 max: %d, min: %d, total: %d, number: %d, avg: %d \n",max1, min1, total1, number1, total1/number1);
+        printf("3 max: %d, min: %d, total: %d, number: %d, avg: %d \n",max3, min3, total3, number3, total3/number3);
+        printf("max: %d, min: %d, total: %d, number: %d, avg: %d \n",max, min, total, number, total/number);
 }
--- docs/machsuite-sort-merge.md
+++ docs/machsuite-sort-merge.md
@@ -9,7 +9,10 @@ Function name changed from `ms_mergesort` to `sort` in all three source files.
 
 # BASELINE
 Data zero copy pragma used for the argument.
-Add tripcount to all 5 loops naively for estimation (only adding for `merge` or `sort` didn't work, but smaller configuration might).
+Add tripcount to all 5 loops (only adding for `merge` or `sort` didn't work, but smaller configuration might).
+1 max: 1024, min: 0, total: 11264, number: 2047, avg: 5
+3 max: 2048, min: 0, total: 22528, number: 2047, avg: 11
+max: 1024, min: 0, total: 2047, number: 11, avg: 186 
 BRAM resource directives commented out because #35. It seems arguments cannot be allocated memory? `WARNING: [SYN 201-303] Cannot apply memory assignment of 'RAM_1P_BRAM' on 'a', which is not an array.`
 Commenting out pipeline directives as they cannot be with variable bounds.
 Loops in merge throw `WARNING: [SCHED 204-65] Unable to satisfy pipeline directive: Function contains subloop(s) not being unrolled.` because `WARNING: [XFORM 203-503] Cannot unroll loop 'merge_label1' (/seashell/buildbot/instance/jobs/uNa_G_ncr7w/code/sort.cpp:7) in function 'merge' completely: variable loop bound.`. 
