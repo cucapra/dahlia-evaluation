4e30f191588231732e71baebaa2705ac60bfac42
Rachit Nigam <rachit.nigam12@gmail.com>
2019-10-12 13:31:28 -0400

    link libs and utils instead of copying

--- experiments/partition/using-pragma/libs/opencl/opencl.mk
+++
@@ -1,7 +0,0 @@
-# Definition of include file locations
-OPENCL_INCLUDE:= $(XILINX_XRT)/include/
-
-opencl_CXXFLAGS=-I$(OPENCL_INCLUDE)
-
-OPENCL_LIB:=$(XILINX_XRT)/lib/
-opencl_LDFLAGS=-L$(OPENCL_LIB) -lOpenCL -lpthread
