ac0f9ea08e9bb54a24915ead4524f2370062849e
Rachit Nigam <rachit.nigam12@gmail.com>
2019-10-16 15:29:38 -0400

    Fix gemm-blocked. - Zero out `prod` array before kernel uses it.

--- baseline/machsuite-gemm-blocked/bench.cpp
+++ baseline/machsuite-gemm-blocked/bench.cpp
@@ -1,6 +1,7 @@
 
 #include <string.h>
 #include "xcl2.hpp"
+#include "helpers.hpp"
 #include <vector>
 #include "gemm.h"
 
@@ -8,81 +9,70 @@ void run_benchmark( void *vargs ) {
   struct bench_args_t *args = (struct bench_args_t *)vargs;
   size_t vector_size_bytes = N*sizeof(TYPE);
   cl_int err;
-  std::vector<TYPE,aligned_allocator<TYPE>> m_in1(4096);
-  std::vector<TYPE,aligned_allocator<TYPE>> m_in2(4096);
-  std::vector<TYPE,aligned_allocator<TYPE>> m_prod(4096);
-  
-  // Copy the test data 
-  for(int i = 0 ; i < 4096 ; i++){
-      m_in1[i] = args->m1[i];
-      m_in2[i] = args->m2[i];
+  std::vector<TYPE,aligned_allocator<TYPE>> m_in1(N);
+  std::vector<TYPE,aligned_allocator<TYPE>> m_in2(N);
+  std::vector<TYPE,aligned_allocator<TYPE>> m_prod(N);
+
+  // Copy the test data
+  for(int i = 0; i < N; i++){
+    m_in1[i] = args->m1[i];
+    m_in2[i] = args->m2[i];
   }
 
   // OPENCL HOST CODE AREA START
-    // get_xil_devices() is a utility API which will find the xilinx
-    // platforms and will return list of devices connected to Xilinx platform
-    std::vector<cl::Device> devices = xcl::get_xil_devices();
-    cl::Device device = devices[0];
+  // get_xil_devices() is a utility API which will find the xilinx
+  // platforms and will return list of devices connected to Xilinx platform
+  std::vector<cl::Device> devices = xcl::get_xil_devices();
+  cl::Device device = devices[0];
 
-    OCL_CHECK(err, cl::Context context(device, NULL, NULL, NULL, &err));
-    OCL_CHECK(err, cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
-    OCL_CHECK(err, std::string device_name = device.getInfo<CL_DEVICE_NAME>(&err)); 
+  OCL_CHECK(err, cl::Context context(device, NULL, NULL, NULL, &err));
+  OCL_CHECK(err, cl::CommandQueue q(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
 
-    // find_binary_file() is a utility API which will search the xclbin file for
-    // targeted mode (sw_emu/hw_emu/hw) and for targeted platforms.
-    std::string binaryFile = xcl::find_binary_file(device_name,"gemm");
+  cl::Kernel krnl_gemm_ncube = helpers::get_kernel(context, device, "gemm", err);
 
-    // import_binary_file() ia a utility API which will load the binaryFile
-    // and will return Binaries.
-    cl::Program::Binaries bins = xcl::import_binary_file(binaryFile);
-    devices.resize(1);
-    OCL_CHECK(err, cl::Program program(context, devices, bins, NULL, &err));
-    OCL_CHECK(err, cl::Kernel krnl_gemm_ncube(program,"gemm", &err));
-
-    // Allocate Buffer in Global Memory
-    // Buffers are allocated using CL_MEM_USE_HOST_PTR for efficient memory and 
-    // Device-to-host communication
-    OCL_CHECK(err,
-              cl::Buffer buffer_in1(context,
-                                    CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-                                    vector_size_bytes,
-                                    m_in1.data(),
-                                    &err));
-    OCL_CHECK(err,
-              cl::Buffer buffer_in2(context,
-                                    CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
-                                    vector_size_bytes,
-                                    m_in2.data(),
-                                    &err));
-    OCL_CHECK(err,
-              cl::Buffer buffer_output(context,
-                                       CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
-                                       vector_size_bytes,
-                                       m_prod.data(),
-                                       &err));
+  // Allocate Buffer in Global Memory
+  // Buffers are allocated using CL_MEM_USE_HOST_PTR for efficient memory and
+  // Device-to-host communication
+  OCL_CHECK(err,
+      cl::Buffer buffer_in1(context,
+        CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+        vector_size_bytes,
+        m_in1.data(),
+        &err));
+  OCL_CHECK(err,
+      cl::Buffer buffer_in2(context,
+        CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY,
+        vector_size_bytes,
+        m_in2.data(),
+        &err));
+  OCL_CHECK(err,
+      cl::Buffer buffer_output(context,
+        CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
+        vector_size_bytes,
+        m_prod.data(),
+        &err));
 
 
-    // Copy input data to device global memory
-    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_in1, buffer_in2},0/* 0 means from host*/));
+  OCL_CHECK(err, err = krnl_gemm_ncube.setArg(0, buffer_in1));
+  OCL_CHECK(err, err = krnl_gemm_ncube.setArg(1, buffer_in2));
+  OCL_CHECK(err, err = krnl_gemm_ncube.setArg(2, buffer_output));
 
-    int size = 4096;
-    OCL_CHECK(err, err = krnl_gemm_ncube.setArg(0, buffer_in1));
-    OCL_CHECK(err, err = krnl_gemm_ncube.setArg(1, buffer_in2));
-    OCL_CHECK(err, err = krnl_gemm_ncube.setArg(2, buffer_output));
+  // Copy input data to device global memory
+  OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_in1, buffer_in2},0));
 
+  // Launch the Kernel
+  // For HLS kernels global and local size is always (1,1,1). So, it is recommended
+  // to always use enqueueTask() for invoking HLS kernel
+  OCL_CHECK(err, err = q.enqueueTask(krnl_gemm_ncube));
 
-    // Launch the Kernel
-    // For HLS kernels global and local size is always (1,1,1). So, it is recommended
-    // to always use enqueueTask() for invoking HLS kernel
-    OCL_CHECK(err, err = q.enqueueTask(krnl_gemm_ncube));
-
-    // Copy Result from Device Global Memory to Host Local Memory
-    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output},CL_MIGRATE_MEM_OBJECT_HOST));
-    q.finish();
+  // Copy Result from Device Global Memory to Host Local Memory
+  OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer_output},CL_MIGRATE_MEM_OBJECT_HOST));
+  q.finish();
   // OPENCL HOST CODE AREA END
-  
-  // Copy results 
-  for(int i = 0 ; i < 4096  ; i++){
-      args->prod[i] = m_prod[i];
+
+  // Copy results
+  for(int i = 0; i < N; i++){
+    args->prod[i] = m_prod[i];
   }
+
 }
--- baseline/machsuite-gemm-blocked/gemm.cpp
+++ baseline/machsuite-gemm-blocked/gemm.cpp
@@ -8,38 +8,42 @@ ASPLOS 1991
 #include "gemm.h"
 
 extern "C" {
-void gemm(TYPE m1[N], TYPE m2[N], TYPE prod[N]){
-#pragma HLS INTERFACE m_axi port = m1 offset = slave bundle = gmem
-#pragma HLS INTERFACE m_axi port = m2 offset = slave bundle = gmem
-#pragma HLS INTERFACE m_axi port = prod offset = slave bundle = gmem
-#pragma HLS INTERFACE s_axilite port = m1 bundle = control
-#pragma HLS INTERFACE s_axilite port = m2 bundle = control
-#pragma HLS INTERFACE s_axilite port = prod bundle = control
-#pragma HLS INTERFACE s_axilite port = return bundle = control
+  void gemm(TYPE m1[N], TYPE m2[N], TYPE prod[N]){
+  #pragma HLS INTERFACE m_axi port = m1 offset = slave bundle = gmem
+  #pragma HLS INTERFACE m_axi port = m2 offset = slave bundle = gmem
+  #pragma HLS INTERFACE m_axi port = prod offset = slave bundle = gmem
+  #pragma HLS INTERFACE s_axilite port = m1 bundle = control
+  #pragma HLS INTERFACE s_axilite port = m2 bundle = control
+  #pragma HLS INTERFACE s_axilite port = prod bundle = control
+  #pragma HLS INTERFACE s_axilite port = return bundle = control
 
-    int i, k, j, jj, kk;
-    int i_row, k_row;
-    TYPE temp_x, mul;
+      int i, k, j, jj, kk;
+      int i_row, k_row;
+      TYPE temp_x, mul;
 
-    loopjj:for (jj = 0; jj < row_size; jj += block_size){
-         #pragma HLS LOOP_TRIPCOUNT min=1 max=64
-        loopkk:for (kk = 0; kk < row_size; kk += block_size){
-             #pragma HLS LOOP_TRIPCOUNT min=1 max=64
-            loopi:for ( i = 0; i < row_size; ++i){
-                 #pragma HLS LOOP_TRIPCOUNT min=1 max=64
-                loopk:for (k = 0; k < block_size; ++k){
-                     #pragma HLS LOOP_TRIPCOUNT min=1 max=64
-                    i_row = i * row_size;
-                    k_row = (k  + kk) * row_size;
-                    temp_x = m1[i_row + k + kk];
-                    loopj:for (j = 0; j < block_size; ++j){
-                         #pragma HLS LOOP_TRIPCOUNT min=1 max=64
-                        mul = temp_x * m2[k_row + j + jj];
-                        prod[i_row + j + jj] += mul;
-                    }
-                }
-            }
-        }
-    }
-}
+      for(int l = 0; l < N; l++) {
+        prod[l] = 0;
+      }
+
+      loopjj:for (jj = 0; jj < row_size; jj += block_size){
+           #pragma HLS LOOP_TRIPCOUNT min=1 max=64
+          loopkk:for (kk = 0; kk < row_size; kk += block_size){
+               #pragma HLS LOOP_TRIPCOUNT min=1 max=64
+              loopi:for ( i = 0; i < row_size; ++i){
+                   #pragma HLS LOOP_TRIPCOUNT min=1 max=64
+                  loopk:for (k = 0; k < block_size; ++k){
+                       #pragma HLS LOOP_TRIPCOUNT min=1 max=64
+                      i_row = i * row_size;
+                      k_row = (k  + kk) * row_size;
+                      temp_x = m1[i_row + k + kk];
+                      loopj:for (j = 0; j < block_size; ++j){
+                           #pragma HLS LOOP_TRIPCOUNT min=1 max=64
+                          mul = temp_x * m2[k_row + j + jj];
+                          prod[i_row + j + jj] += mul;
+                      }
+                  }
+              }
+          }
+      }
+  }
 }
