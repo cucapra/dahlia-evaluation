308c87a51bdb36a447047fc3d422d4d896a98f32
Rachit Nigam <rachit.nigam12@gmail.com>
2019-10-17 08:55:25 -0400

    move cleanup afi script to cerberus config repo

--- _scripts/cleanup_afi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ! hash jq 2>/dev/null; then
-  echo 'Install jq from https://stedolan.github.io/jq/download/'
-  exit 1
-fi
-
-aws ec2 describe-fpga-images --owners 238771226843 | jq '.FpgaImages[] | .FpgaImageId' | xargs -L 1 aws ec2 delete-fpga-image --fpga-image-id
