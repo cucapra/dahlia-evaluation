6854fb4e9ca71d0e010f708aeb4414b626f2708e
Samuel Thomas <sgpthomas@gmail.com>
2019-10-31 13:31:45 -0400

    fixed stencils dahlia rewrite

--- rewrite/machsuite-stencil-stencil2d/stencil.cpp
+++ rewrite/machsuite-stencil-stencil2d/stencil.cpp
@@ -1,29 +1,28 @@
-#include "ap_int.h"
-
-
-#pragma SDS data copy(orig[0:128][0:64])
-void stencil(ap_int<32> orig[128][64], ap_int<32> sol[128][64], ap_int<32> filter[3][3]) {
-  #pragma HLS INTERFACE s_axilite port=orig
-  #pragma HLS INTERFACE s_axilite port=sol
-  #pragma HLS INTERFACE s_axilite port=filter
-  
-  for(int r = 0; r < 126; r++) {
-    for(int c = 0; c < 62; c++) {
-      ap_int<32> temp = 0;
-      
-      for(int k1 = 0; k1 < 3; k1++) {
-        ap_int<32> temp_2 = 0;
-        
-        for(int k2 = 0; k2 < 3; k2++) {
-          ap_int<32> mul = (filter[k1][k2] * orig[(r + k1)][(c + k2)]);
-          
+#include <ap_int.h>
+extern "C" {
+  void stencil(ap_int<32> orig[128][64], ap_int<32> sol[128][64], ap_int<32> filter[3][3]) {
+    #pragma HLS INTERFACE m_axi port=orig offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=orig bundle=control
+    #pragma HLS INTERFACE m_axi port=sol offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=sol bundle=control
+    #pragma HLS INTERFACE m_axi port=filter offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=filter bundle=control
+    #pragma HLS INTERFACE s_axilite port=return bundle=control
+    for(int r = 0; r < 126; r++) {
+      for(int c = 0; c < 62; c++) {
+        ap_int<32> temp = 0;
+        for(int k1 = 0; k1 < 3; k1++) {
+          ap_int<32> temp_2 = 0;
+          for(int k2 = 0; k2 < 3; k2++) {
+            ap_int<32> mul = (filter[k1][k2] * orig[(r + k1)][(c + k2)]);
+            // combiner:
+            temp_2 += mul;
+          }
           // combiner:
-          temp_2 += mul;
+          temp += temp_2;
         }
-        // combiner:
-        temp += temp_2;
+        sol[r][c] = temp;
       }
-      sol[r][c] = temp;
     }
   }
 }
--- rewrite/machsuite-stencil-stencil3d/stencil.cpp
+++ rewrite/machsuite-stencil-stencil3d/stencil.cpp
@@ -1,58 +1,54 @@
-#include "ap_int.h"
-
-
-#pragma SDS data copy(C[0:2])
-void stencil(ap_int<32> C[2], ap_int<32> orig[32][32][16], ap_int<32> sol[32][32][16]) {
-  #pragma HLS INTERFACE s_axilite port=C
-  #pragma HLS INTERFACE s_axilite port=orig
-  #pragma HLS INTERFACE s_axilite port=sol
-  
-  for(int j = 0; j < 32; j++) {
-    for(int k = 0; k < 16; k++) {
-      sol[0][j][k] = orig[0][j][k];
-      //---
-      sol[31][j][k] = orig[31][j][k];
+#include <ap_int.h>
+extern "C" {
+  void stencil(ap_int<32> C[2], ap_int<32> orig[32][32][16], ap_int<32> sol[32][32][16]) {
+    #pragma HLS INTERFACE m_axi port=C offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=C bundle=control
+    #pragma HLS INTERFACE m_axi port=orig offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=orig bundle=control
+    #pragma HLS INTERFACE m_axi port=sol offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=sol bundle=control
+    #pragma HLS INTERFACE s_axilite port=return bundle=control
+    for(int j = 0; j < 32; j++) {
+      for(int k = 0; k < 16; k++) {
+        sol[0][j][k] = orig[0][j][k];
+        //---
+        sol[31][j][k] = orig[31][j][k];
+      }
     }
-  }
-  //---
-  for(int i = 1; i < 31; i++) {
-    for(int k = 0; k < 16; k++) {
-      sol[i][0][k] = orig[i][0][k];
-      //---
-      sol[i][31][k] = orig[i][31][k];
+    //---
+    for(int i = 1; i < 31; i++) {
+      for(int k = 0; k < 16; k++) {
+        sol[i][0][k] = orig[i][0][k];
+        //---
+        sol[i][31][k] = orig[i][31][k];
+      }
     }
-  }
-  //---
-  for(int i = 1; i < 31; i++) {
-    for(int j = 1; j < 31; j++) {
-      sol[i][j][0] = orig[i][j][0];
-      //---
-      sol[i][j][15] = orig[i][j][15];
+    //---
+    for(int i = 1; i < 31; i++) {
+      for(int j = 1; j < 31; j++) {
+        sol[i][j][0] = orig[i][j][0];
+        //---
+        sol[i][j][15] = orig[i][j][15];
+      }
     }
-  }
-  //---
-  for(int i = 1; i < 31; i++) {
-    for(int j = 1; j < 31; j++) {
-      for(int k = 1; k < 15; k++) {
-        ap_int<32> temp1 = (orig[i][j][k] * C[0]);
-        
-        //---
-        ap_int<32> temp2 = ((orig[(i + 1)][j][k] * C[1]) + temp1);
-        
-        //---
-        ap_int<32> temp3 = ((orig[(i - 1)][j][k] * C[1]) + temp2);
-        
-        //---
-        ap_int<32> temp4 = ((orig[i][(j + 1)][k] * C[1]) + temp3);
-        
-        //---
-        ap_int<32> temp5 = ((orig[i][(j - 1)][k] * C[1]) + temp4);
-        
-        //---
-        ap_int<32> temp6 = ((orig[i][j][(k + 1)] * C[1]) + temp5);
-        
-        //---
-        sol[i][j][k] = ((orig[i][j][(k - 1)] * C[1]) + temp6);
+    //---
+    for(int i = 1; i < 31; i++) {
+      for(int j = 1; j < 31; j++) {
+        for(int k = 1; k < 15; k++) {
+          ap_int<32> temp1 = (orig[i][j][k] * C[0]);
+          //---
+          ap_int<32> temp2 = ((orig[(i + 1)][j][k] * C[1]) + temp1);
+          //---
+          ap_int<32> temp3 = ((orig[(i - 1)][j][k] * C[1]) + temp2);
+          //---
+          ap_int<32> temp4 = ((orig[i][(j + 1)][k] * C[1]) + temp3);
+          //---
+          ap_int<32> temp5 = ((orig[i][(j - 1)][k] * C[1]) + temp4);
+          //---
+          ap_int<32> temp6 = ((orig[i][j][(k + 1)] * C[1]) + temp5);
+          //---
+          sol[i][j][k] = ((orig[i][j][(k - 1)] * C[1]) + temp6);
+        }
       }
     }
   }
