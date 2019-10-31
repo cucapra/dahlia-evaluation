28cf3bd1b934c1c076488405727a1dc6691cde64
Samuel Thomas <sgpthomas@gmail.com>
2019-10-31 13:24:17 -0400

    fixed spmv-ellpack dahlia rewrite

--- rewrite/machsuite-spmv-ellpack/spmv.cpp
+++ rewrite/machsuite-spmv-ellpack/spmv.cpp
@@ -1,20 +1,21 @@
-#include "ap_int.h"
-
-
-#pragma SDS data copy(nzval[0:494][0:10])
-#pragma SDS data zero_copy(out[0:494])
-void spmv(double nzval[494][10], ap_int<32> cols[494][10], double vec[494], double out[494]) {
-  #pragma HLS INTERFACE s_axilite port=nzval
-  #pragma HLS INTERFACE s_axilite port=cols
-  #pragma HLS INTERFACE s_axilite port=vec
-  #pragma HLS INTERFACE s_axilite port=out
-  
-  for(int i = 0; i < 494; i++) {
-    for(int j = 0; j < 10; j++) {
-      double si = (nzval[i][j] * vec[cols[i][j]]);
-      
-      // combiner:
-      out[i] += si;
+#include <ap_int.h>
+extern "C" {
+  void spmv(double nzval[494][10], ap_int<32> cols[494][10], double vec[494], double out[494]) {
+    #pragma HLS INTERFACE m_axi port=nzval offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=nzval bundle=control
+    #pragma HLS INTERFACE m_axi port=cols offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=cols bundle=control
+    #pragma HLS INTERFACE m_axi port=vec offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=vec bundle=control
+    #pragma HLS INTERFACE m_axi port=out offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=out bundle=control
+    #pragma HLS INTERFACE s_axilite port=return bundle=control
+    for(int i = 0; i < 494; i++) {
+      for(int j = 0; j < 10; j++) {
+        double si = (nzval[i][j] * vec[cols[i][j]]);
+        // combiner:
+        out[i] += si;
+      }
     }
   }
 }
