2deee3b6b7b4f9a098bedaeeca22eaf10eb01331
Samuel Thomas <sgpthomas@gmail.com>
2019-10-31 12:49:53 -0400

    Fixed gemm, kmp for dahlia rewrites

--- rewrite/machsuite-gemm-ncubed/gemm.cpp
+++ rewrite/machsuite-gemm-ncubed/gemm.cpp
@@ -1,23 +1,30 @@
-#include "ap_int.h"
+#include <ap_int.h>
 
 
-#pragma SDS data copy(m1[0:64][0:64])
-void gemm(double m1[64][64], double m2[64][64], double prod[64][64]) {
-  #pragma HLS INTERFACE s_axilite port=m1
-  #pragma HLS INTERFACE s_axilite port=m2
-  #pragma HLS INTERFACE s_axilite port=prod
-  
-  for(int i = 0; i < 64; i++) {
-    for(int j = 0; j < 64; j++) {
-      double sum = 0.0;
-      
-      for(int k = 0; k < 64; k++) {
-        double mult = (m1[i][k] * m2[k][j]);
+
+extern "C" {
+  void gemm(double m1[64][64], double m2[64][64], double prod[64][64]) {
+    #pragma HLS INLINE
+    #pragma HLS INTERFACE m_axi port=m1 offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=m1 bundle=control
+    #pragma HLS INTERFACE m_axi port=m2 offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=m2 bundle=control
+    #pragma HLS INTERFACE m_axi port=prod offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=prod bundle=control
+    #pragma HLS INTERFACE s_axilite port=return bundle=control
+    
+    for(int i = 0; i < 64; i++) {
+      for(int j = 0; j < 64; j++) {
+        double sum = 0.0;
         
-        // combiner:
-        sum += mult;
+        for(int k = 0; k < 64; k++) {
+          double mult = (m1[i][k] * m2[k][j]);
+          
+          // combiner:
+          sum += mult;
+        }
+        prod[i][j] = sum;
       }
-      prod[i][j] = sum;
     }
   }
 }
--- rewrite/machsuite-kmp/kmp.cpp
+++ rewrite/machsuite-kmp/kmp.cpp
@@ -1,7 +1,9 @@
-#include "ap_int.h"
+#include <ap_int.h>
 
 void cpf(ap_int<8> pattern[4], ap_int<32> kmpNext[4]) {
+  #pragma HLS INLINE
   
+  #pragma HLS INTERFACE s_axilite port=return bundle=control
   
   ap_int<32> k = 0;
   
@@ -23,10 +25,8 @@ void cpf(ap_int<8> pattern[4], ap_int<32> kmpNext[4]) {
       q_val = pattern[q];
     }
     //---
-    if((k_val == q_val)){
+    if ((k_val == q_val)) {
       k = (k + 1);
-    } else{
-      
     }
     kmpNext[q] = k;
   }
@@ -34,37 +34,41 @@ void cpf(ap_int<8> pattern[4], ap_int<32> kmpNext[4]) {
 #pragma SDS data copy(pattern[0:PATTERN_SIZE])
 #pragma SDS data zero_copy(kmpNext[0:PATTERN_SIZE], n_matches)
 #pragma SDS data zero_copy(input[0:STRING_SIZE])
-void kmp(ap_int<8> pattern[4], ap_int<8> input[32411], ap_int<32> kmpNext[4], ap_int<32> n_matches[1]) {
-  #pragma HLS INTERFACE s_axilite port=pattern
-  #pragma HLS INTERFACE s_axilite port=input
-  #pragma HLS INTERFACE s_axilite port=kmpNext
-  #pragma HLS INTERFACE s_axilite port=n_matches
-  
-  n_matches[0] = 0;
-  cpf(pattern, kmpNext);
-  //---
-  ap_int<32> q = 0;
-  
-  for(int i = 0; i < 32411; i++) {
-    #pragma HLS loop_tripcount max=32411 min=0
-    while(((q > 0) && (pattern[q] != input[i]))) {
-      #pragma HLS loop_tripcount max=1 min=0
-      q = kmpNext[q];
-    }
+extern "C" {
+  void kmp(ap_int<8> pattern[4], ap_int<8> input[32411], ap_int<32> kmpNext[4], ap_int<32> n_matches[1]) {
+    #pragma HLS INTERFACE m_axi port=pattern offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=pattern bundle=control
+    #pragma HLS INTERFACE m_axi port=input offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=input bundle=control
+    #pragma HLS INTERFACE m_axi port=kmpNext offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=kmpNext bundle=control
+    #pragma HLS INTERFACE m_axi port=n_matches offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=n_matches bundle=control
+    
+    #pragma HLS INTERFACE s_axilite port=return bundle=control
+    
+    n_matches[0] = 0;
+    cpf(pattern, kmpNext);
     //---
-    if((pattern[q] == input[i])){
-      q = (q + 1);
-    } else{
-      
-    }
-    if((q >= 4)){
-      ap_int<32> temp = n_matches[0];
-      
+    ap_int<32> q = 0;
+    
+    for(int i = 0; i < 32411; i++) {
+      #pragma HLS loop_tripcount max=32411 min=0
+      while(((q > 0) && (pattern[q] != input[i]))) {
+        #pragma HLS loop_tripcount max=1 min=0
+        q = kmpNext[q];
+      }
       //---
-      n_matches[0] = (temp + 1);
-      q = kmpNext[(q - 1)];
-    } else{
-      
+      if ((pattern[q] == input[i])) {
+        q = (q + 1);
+      }
+      if ((q >= 4)) {
+        ap_int<32> temp = n_matches[0];
+        
+        //---
+        n_matches[0] = (temp + 1);
+        q = kmpNext[(q - 1)];
+      }
     }
   }
 }
