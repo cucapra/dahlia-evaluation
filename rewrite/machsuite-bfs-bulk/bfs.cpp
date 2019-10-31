fc260313bf9b0c38f8e68c91cdf2294339e1d556
Samuel Thomas <sgpthomas@gmail.com>
2019-10-31 15:01:06 -0400

    fixed dahlia rewrite for bfs-bulk

--- rewrite/machsuite-bfs-bulk/bfs.cpp
+++ rewrite/machsuite-bfs-bulk/bfs.cpp
@@ -1,67 +1,53 @@
-#include "ap_int.h"
-
-typedef struct {
-  ap_uint<64> src;
-  ap_uint<64> dst;
-} edge_t;
-typedef struct {
-  ap_uint<64> edge_begin;
-  ap_uint<64> edge_end;
-} node_t;
-#pragma SDS data copy(nodes[0:256])
-#pragma SDS data zero_copy(level[0:256])
-void bfs(node_t nodes[256], edge_t edges[4096], ap_uint<64> starting_node, ap_int<8> level[256], ap_uint<64> level_counts[10]) {
-  #pragma HLS INTERFACE s_axilite port=nodes
-  #pragma HLS INTERFACE s_axilite port=edges
-  
-  #pragma HLS INTERFACE s_axilite port=level
-  #pragma HLS INTERFACE s_axilite port=level_counts
-  
-  ap_int<8> MAX_LEVEL = 127;
-  
-  level[starting_node] = 0;
-  level_counts[0] = 1;
-  //---
-  for(int horizon = 0; horizon < 10; horizon++) {
-    #pragma HLS loop_tripcount max=10 min=1
-    ap_uint<64> cnt = 0;
-    
-    for(int n = 0; n < 256; n++) {
-      ap_int<8> tmp_l = level[n];
-      
-      //---
-      if((tmp_l == horizon)){
-        ap_uint<64> e = nodes[n].edge_begin;
-        
+#include <ap_int.h>
+extern "C" {
+  void bfs(ap_uint<64> nodes_edge_begin[256], ap_uint<64> nodes_edge_end[256], ap_uint<64> edges_src[4096], ap_uint<64> edges_dst[4096], ap_uint<64> starting_node, ap_int<8> level[256], ap_uint<64> level_counts[10]) {
+    #pragma HLS INTERFACE m_axi port=nodes_edge_begin offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=nodes_edge_begin bundle=control
+    #pragma HLS INTERFACE m_axi port=nodes_edge_end offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=nodes_edge_end bundle=control
+    #pragma HLS INTERFACE m_axi port=edges_src offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=edges_src bundle=control
+    #pragma HLS INTERFACE m_axi port=edges_dst offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=edges_dst bundle=control
+    #pragma HLS INTERFACE s_axilite port=starting_node bundle=control
+    #pragma HLS INTERFACE m_axi port=level offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=level bundle=control
+    #pragma HLS INTERFACE m_axi port=level_counts offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=level_counts bundle=control
+    #pragma HLS INTERFACE s_axilite port=return bundle=control
+    ap_int<8> MAX_LEVEL = 127;
+    level[starting_node] = 0;
+    level_counts[0] = 1;
+    //---
+    for(int horizon = 0; horizon < 10; horizon++) {
+      #pragma HLS loop_tripcount max=10 min=1
+      ap_uint<64> cnt = 0;
+      for(int n = 0; n < 256; n++) {
+        ap_int<8> tmp_l = level[n];
         //---
-        ap_uint<64> tmp_end = nodes[n].edge_end;
-        
-        //---
-        while((e < tmp_end)) {
-          #pragma HLS loop_tripcount avg=17
-          ap_uint<64> tmp_dst = edges[e].dst;
-          
-          ap_int<8> tmp_level = level[tmp_dst];
-          
+        if ((tmp_l == horizon)) {
+          ap_uint<64> e = nodes_edge_begin[n];
           //---
-          if((tmp_level == MAX_LEVEL)){
-            level[tmp_dst] = (horizon + 1);
+          ap_uint<64> tmp_end = nodes_edge_end[n];
+          //---
+          while((e < tmp_end)) {
+            #pragma HLS loop_tripcount avg=17
+            ap_uint<64> tmp_dst = edges_dst[e];
+            ap_int<8> tmp_level = level[tmp_dst];
             //---
-            cnt = (cnt + 1);
-          } else{
-            
+            if ((tmp_level == MAX_LEVEL)) {
+              level[tmp_dst] = (horizon + 1);
+              //---
+              cnt = (cnt + 1);
+            }
+            e = (e + 1);
           }
-          e = (e + 1);
         }
-      } else{
-        
       }
-    }
-    level_counts[(horizon + 1)] = cnt;
-    if((cnt == 0)){
-      horizon = 10;
-    } else{
-      
+      level_counts[(horizon + 1)] = cnt;
+      if ((cnt == 0)) {
+        horizon = 10;
+      }
     }
   }
 }
