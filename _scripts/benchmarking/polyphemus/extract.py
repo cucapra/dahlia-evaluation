5f7fba34cf267a76cd0d1b9700cbacbb1a5fa8d6
Rachit Nigam <rachit.nigam12@gmail.com>
2019-11-05 13:26:32 -0500

    factor out extraction and summarize scripts from cli tools

---
+++ _scripts/benchmarking/polyphemus/extract.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import logging
+import json
+import requests
+import argparse
+
+from benchmarking.summary import collect_configs
+
+RESULTS_FILE = "results.json"  # Final, aggregated results for the batch.
+DOWNLOAD_DIR = "raw"  # Subdirectory where we download files for extraction.
+FAILURE_FILE = "failure_extract.txt"  # Job IDs we could not extract.
+
+
+def _download(url, path, chunk_size=4096):
+    """Download the file at `url` to `path`.
+    """
+    with requests.get(url, stream=True) as res:
+        res.raise_for_status()
+        with open(path, 'wb') as f:
+            for chunk in res.iter_content(chunk_size=chunk_size):
+                if chunk:
+                    f.write(chunk)
+
+
+def get_metadata(job_id):
+    """Download the configuration JSON located at $BUILDBOT/jobs/.
+    Return None if the download fails.
+    """
+    url = '{}/jobs/{}'.format(common.buildbot_url(), job_id)
+    try:
+        with requests.get(url) as res:
+            res.raise_for_status()
+            return res.json()
+    except requests.HTTPError:
+        return None
+
+
+def download_files(base_dir, job_id, file_config):
+    """Download specified files for the job from
+    $BUILDBOT/jobs/<job_id>/files/<files>.
+    to base_dir/job_id and runs the 'collect' method for each file.
+
+    Return a Boolean success flag (which is false if there was at least
+    one failure) and a dict of outputs (the result of calling `collect` on
+    each file path).
+    """
+    success = True
+    out_data = {}
+
+    # Create the job download directory
+    job_path = os.path.join(base_dir, job_id)
+    os.makedirs(job_path, exist_ok=True)
+
+    assert file_config != [], "No collect configurations provided"
+
+    logging.info(
+        "Extracting files for {} in {} for collections {}.".format(
+            job_id,
+            job_path,
+            ', '.join(map(lambda conf: conf.key, file_config))))
+
+    for conf in file_config:
+        url = '{}/jobs/{}/files/{}'.format(
+            common.buildbot_url(),
+            job_id,
+            conf.file,
+        )
+        local_name = os.path.join(job_path, os.path.basename(conf.file))
+        try:
+            _download(url, local_name)
+        except requests.HTTPError as exc:
+            success = False
+            logging.error(
+                'Failed to download {} for job {}: {}'.format(
+                    conf.file, job_id, exc))
+        else:
+            try:
+                out_data[conf.key] = conf.collect(local_name)
+            except Exception as err:
+                success = False
+                logging.error(
+                    'Failed to parse {} for job {}.'.format(
+                        conf.file, job_id))
+                logging.error(err)
+
+    return success, out_data
+
+
+def extract_job(batch_dir, job_id, static_collects, dync_collects):
+    """Extract information about a single job. Return a dict of
+    information to include about it. The dict *at least* has an 'ok'
+    attribute, indicating whether the job is completely ready.
+    """
+
+    job = get_metadata(job_id)
+    if not job:
+        logging.error('Could not get information for %s.', job_id)
+        return {'ok': False, 'error': 'job lookup failed'}
+
+    hwname = job['config']['hwname']
+    out = {
+        'ok': True,
+        'job': job,
+        'bench': hwname,
+    }
+
+    if job['state'] != 'done':
+        logging.error('Job %s in state `%s`.', job_id, job['state'])
+        out.update({
+            'ok': False,
+            'error': 'job in state {}'.format(job['state']),
+        })
+
+    # Collections for this job
+    collections = list(static_collects)
+    dynamic_collections = list(dyn_collects)
+
+    # Get the list of files for the job.
+    if len(dynamic_collections) > 0:
+        files_url = '{}/jobs/{}/files'.format(common.buildbot_url(), job_id)
+        with requests.get(files_url) as res:
+            res.raise_for_status()
+            file_list = res.json()
+
+        # Find paths for dynamic collections.
+        for file_path in file_list:
+            for idx, collect in enumerate(dynamic_collections):
+                if not isinstance(collect.file, str) and collect.file(file_path):
+                    dynamic_collections.pop(idx)
+                    collections.append(collect_configs.CollectConfig(
+                        key=collect.key,
+                        file=collect.file(file_path),
+                        collect=collect.collect,
+                    ))
+
+        # Errors for dynamic_collections that didn't find matching file_paths.
+        for collect in dynamic_collections:
+            logging.error(
+                "Failed to find file for collection '%s' in job %s", collect.key, job_id)
+
+    # Download files and extract results.
+    success, res_data = download_files(
+        os.path.join(batch_dir, DOWNLOAD_DIR),
+        job_id,
+        collections,
+    )
+
+    out['results'] = res_data
+    if out['ok'] and not success:
+        logging.error('Some data extraction failed.')
+        out.update({
+            'ok': False,
+            'error': 'data extraction failed',
+        })
+
+    return out
+
+
+def extract_batch(batch_dir, static_collects, dyn_collects):
+    # Load the list of job IDs for this batch.
+    job_file = os.path.join(batch_dir, common.JOBS_FILE)
+    if not os.path.isfile(job_file):
+        raise Exception("{} is not a file".format(job_file))
+    with open(job_file, "r") as jobs:
+        job_ids = jobs.read().strip().split('\n')
+
+    failed_jobs = set()
+    json_data = {}
+
+    for job_id in job_ids:
+        logging.info('Extracting %s', job_id)
+        data = extract_job(batch_dir, job_id, static_collects, dyn_collects)
+        json_data[job_id] = data
+        if not data['ok']:
+            failed_jobs.add(job_id)
+
+    # Log failed jobs to failure.
+    if failed_jobs:
+        logging.warning('Failed jobs: %s', ', '.join(failed_jobs))
+        with open(os.path.join(batch_dir, FAILURE_FILE), 'w') as failed:
+            for job_id in failed_jobs:
+                print(job_id, file=failed)
+
+    # Write out results.
+    report_path = os.path.join(batch_dir, RESULTS_FILE)
+    with open(report_path, 'w') as report:
+        logging.info("Writing data to {}".format(RESULTS_FILE))
+        json.dump(json_data, report, sort_keys=True, indent=2)
---
+++ _scripts/benchmarking/summary/summarize.py
@@ -0,0 +1,92 @@
+import json
+import csv
+import sys
+import os
+import argparse
+import numpy as np
+import common
+
+from collections import defaultdict, ChainMap
+
+from . import summary_configs
+
+def access_obj(obj, acc):
+    """Given `acc` of the form `x:y:z`, access the field `obj.x.y.z`
+    """
+    accessors = acc.split(':')
+    ret = obj
+    while accessors and ret:
+        ret = ret.get(accessors.pop(0))
+    return ret
+
+
+def get_runtime(job_results, runtime_key="time(ms)"):
+    """Collect statistics from the runtime information array.
+    """
+
+    runtime_arr = [float(r[runtime_key]) for r in job_results['results']['runtime']
+                   if r[runtime_key] != runtime_key]
+
+    return {
+        'runtime_avg': np.mean(runtime_arr),
+        'runtime_std': np.std(runtime_arr),
+    }
+
+def summarize_one(job_results, field_map, runtime):
+    bench = job_results['bench']
+
+    out = {
+        # Identify the job.
+        'status': 'ok' if job_results['ok'] else 'error',
+    }
+
+    # Abort early if we don't have statistics.
+    if 'results' not in job_results:
+        return out
+
+    # Try to provide a better status message.
+    if 'log' in job_results['results']:
+        log = job_results['results']['log']
+        for key, present in log.items():
+            if present:
+                out['status'] = key
+                break
+
+    fields = {k: access_obj(job_results, v) for k, v in field_map.items()}
+
+    if runtime:
+        runtime_fields = get_runtime(job_results)
+    else:
+        runtime_fields = {}
+
+    return { **out, **fields, **runtime_fields }
+
+
+def find_missing(summaries, ref_version, seeking_version, field_map, key_name='version'):
+    """Given a list of result summaries, find all the benchmarks that
+    have an entry for `ref_version` but *not* for `seeking_version`
+    for the given `key_name`.
+    """
+    ref_benchmaks = {s['bench'] for s in summaries
+                     if s[key_name] == ref_version}
+    seeking_benchmarks = {s['bench'] for s in summaries
+                          if s[key_name] == seeking_version}
+    return ref_benchmaks - seeking_benchmarks
+
+
+def summarize_json(results_json, field_map, runtime, out_csv):
+    # Load JSON data.
+    with open(results_json) as f:
+        results_data = json.load(f)
+
+    # Generate summaries.
+    out = [summarize_one(j, field_map, runtime) for j in results_data.values()]
+
+    # Dump CSV output.
+    headers = [f for f in out[0].keys()]
+    csv_filename = os.path.join(os.path.dirname(results_json), out_csv)
+    with open(csv_filename, 'w') as f:
+        writer = csv.DictWriter(f, headers)
+        writer.writeheader()
+        for res in out:
+            writer.writerow(defaultdict(str, res))  # Silently allow blanks.
