2e453bc9a5d001d08f55500e0363136b5c2064b3
Rachit Nigam <rachit.nigam12@gmail.com>
2019-11-05 14:19:28 -0500

    add data for dot dse

---
+++ experiments/dot-big-dse/extract.mk
@@ -0,0 +1,8 @@
+
+data/summary.csv: data/results.json
+	rm -f data/failure_extract.txt
+	../../_scripts/summarize.py data/results.json -k key.json -c sda_est -R
+
+
+data/results.json: data/jobs.txt
+	./get-files.py
