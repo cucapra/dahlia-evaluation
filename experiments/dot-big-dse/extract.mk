c40619b1953334f281464a6f93c0494132bbf2e5
Rachit Nigam <rachit.nigam12@gmail.com>
2019-11-06 21:48:34 -0500

    data from extended DSE run

--- experiments/dot-big-dse/extract.mk
+++ experiments/dot-big-dse/extract.mk
@@ -5,3 +5,9 @@ data/summary.csv: data/results.json
 
 data/results.json: data/jobs.txt
 	./get-files.py
+
+full-synth/results.json: full-synth/jobs.txt
+	../../_scripts/extract.py full-synth/ -c routed_util dse_template runtime
+
+full-synth/summary.csv: full-synth/results.json
+	../../_scripts/summarize.py full-synth/results.json -k key.json -c routed_util default
--- experiments/dot-big-dse/get-files.py
+++ experiments/dot-big-dse/get-files.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
+import sys
+
 from benchmarking import common
 from benchmarking.summary import extracting, collect_configs
 from benchmarking.polyphemus.extract import extract_batch
 
-OUT_DIR = 'data'
-
 if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        print('Script requires a path to a directory for the batch.\n'
+              'Usage: ./get-files.py <batch-dir>')
+        sys.exit(1)
 
     static_est = collect_configs.CollectConfig(
         key='sda_est',
@@ -16,4 +21,4 @@ if __name__ == '__main__':
 
     common.logging_setup()
     static_collects = [ static_est, collect_configs.VALID_COLLECTS['dse_template'] ]
-    extract_batch(OUT_DIR, static_collects, [])
+    extract_batch(sys.argv[1], static_collects, [])
