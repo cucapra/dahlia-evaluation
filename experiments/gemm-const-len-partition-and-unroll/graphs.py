dc96cb5bbd2647a28242997b9dee18e65f96b81e
Rachit Nigam <rachit.nigam12@gmail.com>
2019-11-12 13:50:16 -0500

    graphing function update

--- experiments/gemm-const-len-partition-and-unroll/graphs.py
+++ experiments/gemm-const-len-partition-and-unroll/graphs.py
@@ -3,6 +3,7 @@
 import pandas as pd
 from benchmarking.plotting import plots
 
+
 def make_plots():
     x_key = 'partition'
     y_keys = [
@@ -13,13 +14,20 @@ def make_plots():
     ]
     df = pd.read_csv('./summary.csv').sort_values(by=x_key).reset_index()
 
+    def group(idx):
+        row = df.iloc[idx]
+        if row.status == 'error':
+            return 2
+        return int(16 % df.iloc[idx][x_key] == 0)
+
     plots.make_absolute_plots(
         df,
         x_key,
         y_keys,
         # group_by = lambda row: 1 if 8 % row.partition == 0 else 0,
-        group_by = lambda idx: int(8 % df.iloc[idx][x_key] == 0),
-        group_labels = ['Unpredictable points', 'Predictable points'],
+        group_by = group,
+        group_labels = ['Unpredictable points', 'Predictable points', 'Incorrect hardware'],
+        group_markers = ['o', 'o', 'x'],
         x_label = 'Partitioning and Unrolling factor',
         fig_prefix = 'const-len-partition-and-unroll')
 
