ceae6cbb209a72d07d17f68b41af24d5f3495207
Rachit Nigam <rachit.nigam12@gmail.com>
2019-11-12 13:18:46 -0500

    script for const-len experiment

--- experiments/gemm-const-len-partition-and-unroll/extract.mk
+++ experiments/gemm-const-len-partition-and-unroll/extract.mk
@@ -1,9 +1,1 @@
-misaligned-partition-absolute-lut-used.pdf misaligned-partition-absolute-runtime-avg.pdf: summary.csv
-	jupyter nbconvert --execute analysis.ipynb
-
-summary.csv: results.json
-	../../_scripts/summarize.py -k key.json results.json -c routed_util
-
-
-results.json: jobs.txt
-	../../_scripts/extract.py ./ -c dse_template routed_util runtime
+../../_common/experiment-common/extract.mk
