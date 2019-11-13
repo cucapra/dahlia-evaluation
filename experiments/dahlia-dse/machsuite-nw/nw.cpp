49dc09444da8b238c04e253ccbe6b42c8e8d78be
Adrian Sampson <adrian@radbox.org>
2019-11-13 18:01:28 -0500

    First-draft optimized nw
    Who knows whether it works? Not I!

--- experiments/dahlia-dse/machsuite-nw/nw.cpp
+++ experiments/dahlia-dse/machsuite-nw/nw.cpp
@@ -1,21 +1,34 @@
+// git.status = clean, build.date = Wed Nov 13 17:14:51 EST 2019, git.hash = bac29f5
 #include <ap_int.h>
-#pragma SDS data copy(SEQA[0:128])
-#pragma SDS data zero_copy(M[0:16641], ptr[0:16641])
 extern "C" {
-  void nw(ap_int<8> SEQA[128], ap_int<8> SEQB[128], ap_int<8> alignedA[256], ap_int<8> alignedB[256], ap_int<32> M[16641], ap_int<8> ptr[16641]) {
-    #pragma HLS INTERFACE m_axi port=SEQA offset=slave bundle=gmem
-    #pragma HLS INTERFACE s_axilite port=SEQA bundle=control
-    #pragma HLS INTERFACE m_axi port=SEQB offset=slave bundle=gmem
-    #pragma HLS INTERFACE s_axilite port=SEQB bundle=control
+  void nw(ap_int<8> SEQA_interface[128], ap_int<8> SEQB_interface[128], ap_int<8> alignedA[256], ap_int<8> alignedB[256], ap_int<32> M_interface[16641], ap_int<8> ptr_interface[16641]) {
+    #pragma HLS INTERFACE m_axi port=SEQA_interface offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=SEQA_interface bundle=control
+    #pragma HLS INTERFACE m_axi port=SEQB_interface offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=SEQB_interface bundle=control
     #pragma HLS INTERFACE m_axi port=alignedA offset=slave bundle=gmem
     #pragma HLS INTERFACE s_axilite port=alignedA bundle=control
     #pragma HLS INTERFACE m_axi port=alignedB offset=slave bundle=gmem
     #pragma HLS INTERFACE s_axilite port=alignedB bundle=control
-    #pragma HLS INTERFACE m_axi port=M offset=slave bundle=gmem
-    #pragma HLS INTERFACE s_axilite port=M bundle=control
-    #pragma HLS INTERFACE m_axi port=ptr offset=slave bundle=gmem
-    #pragma HLS INTERFACE s_axilite port=ptr bundle=control
+    #pragma HLS INTERFACE m_axi port=M_interface offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=M_interface bundle=control
+    #pragma HLS INTERFACE m_axi port=ptr_interface offset=slave bundle=gmem
+    #pragma HLS INTERFACE s_axilite port=ptr_interface bundle=control
     #pragma HLS INTERFACE s_axilite port=return bundle=control
+    ap_int<8> SEQA[128];
+    #pragma HLS resource variable=SEQA core=RAM_1P_BRAM
+    #pragma HLS ARRAY_PARTITION variable=SEQA cyclic factor=2 dim=1
+    ap_int<8> SEQB[128];
+    #pragma HLS resource variable=SEQB core=RAM_1P_BRAM
+    #pragma HLS ARRAY_PARTITION variable=SEQB cyclic factor=2 dim=1
+    ap_int<32> M[130][130];
+    #pragma HLS resource variable=M core=RAM_1P_BRAM
+    #pragma HLS ARRAY_PARTITION variable=M cyclic factor=2 dim=1
+    #pragma HLS ARRAY_PARTITION variable=M cyclic factor=2 dim=2
+    ap_int<8> ptr[130][130];
+    #pragma HLS resource variable=ptr core=RAM_1P_BRAM
+    #pragma HLS ARRAY_PARTITION variable=ptr cyclic factor=2 dim=1
+    #pragma HLS ARRAY_PARTITION variable=ptr cyclic factor=2 dim=2
     ap_int<32> MATCH_SCORE = 1;
     ap_int<32> MISMATCH_SCORE = (0 - 1);
     ap_int<32> GAP_SCORE = (0 - 1);
@@ -24,33 +37,59 @@ extern "C" {
     ap_int<8> SKIPB = 60;
     ap_int<8> DASH = 45;
     ap_int<8> UNDERSCORE = 95;
-    for(int a_idx = 0; a_idx < 129; a_idx++) {
-      #pragma HLS loop_tripcount max=128 min=0
-      M[a_idx] = (a_idx * GAP_SCORE);
+    
+    
+    
+    
+    for(int i = 0; i < 128; i++) {
+      SEQA[i] = SEQA_interface[i];
+      SEQB[i] = SEQB_interface[i];
+    }
+    for(int i = 0; i < 129; i++) {
+      for(int j = 0; j < 129; j++) {
+        M[i][j] = M_interface[((i * 129) + j)];
+        ptr[i][j] = ptr_interface[((i * 129) + j)];
+      }
     }
     //---
-    for(int b_idx = 0; b_idx < 129; b_idx++) {
+    for(int a_idx = 0; a_idx < 130; a_idx++) {
+      #pragma HLS UNROLL factor=2 skip_exit_check
+      
       #pragma HLS loop_tripcount max=128 min=0
-      M[(b_idx * 129)] = (b_idx * GAP_SCORE);
+      M[0][a_idx] = (a_idx * GAP_SCORE);
     }
     //---
+    for(int b_idx = 0; b_idx < 130; b_idx++) {
+      #pragma HLS UNROLL factor=2 skip_exit_check
+      
+      #pragma HLS loop_tripcount max=128 min=0
+      M[b_idx][0] = (b_idx * GAP_SCORE);
+    }
+    //---
+    
+    
+    
+    
+    
     for(int b_idx = 1; b_idx < 129; b_idx++) {
+      #pragma HLS UNROLL factor=2 skip_exit_check
+      
       #pragma HLS loop_tripcount max=128 min=0
       for(int a_idx = 1; a_idx < 129; a_idx++) {
+        #pragma HLS UNROLL factor=2 skip_exit_check
+        
         #pragma HLS loop_tripcount max=128 min=0
         ap_int<32> score = 0;
-        if ((SEQA[(a_idx - 1)] == SEQB[(b_idx - 1)])) {
+        if ((SEQA[((1 * -1) + a_idx)] == SEQB[((1 * -1) + b_idx)])) {
           score = MATCH_SCORE;
         } else {
           score = MISMATCH_SCORE;
         }
-        ap_int<32> row_up = ((b_idx - 1) * 129);
-        ap_int<32> row = (b_idx * 129);
-        ap_int<32> up_left = (M[((row_up + a_idx) - 1)] + score);
+        ap_int<32> up_left = (M[(-1 + b_idx)][(-1 + a_idx)] + score);
         //---
-        ap_int<32> up = (M[(row_up + a_idx)] + GAP_SCORE);
+        ap_int<32> up = (M[(-1 + b_idx)][a_idx] + GAP_SCORE);
         //---
-        ap_int<32> left = (M[((row + a_idx) - 1)] + GAP_SCORE);
+        ap_int<32> left = (M[b_idx][((1 * -1) + a_idx)] + GAP_SCORE);
         ap_int<32> max = 0;
         if (((up_left >= up) && (up_left >= left))) {
           max = up_left;
@@ -62,14 +101,14 @@ extern "C" {
           }
         }
         //---
-        M[(row + a_idx)] = max;
+        M[b_idx][a_idx] = max;
         if ((max == left)) {
-          ptr[(row + a_idx)] = SKIPB;
+          ptr[b_idx][a_idx] = SKIPB;
         } else {
           if ((max == up)) {
-            ptr[(row + a_idx)] = SKIPA;
+            ptr[b_idx][a_idx] = SKIPA;
           } else {
-            ptr[(row + a_idx)] = ALIGN;
+            ptr[b_idx][a_idx] = ALIGN;
           }
         }
       }
@@ -79,16 +118,19 @@ extern "C" {
     ap_int<32> b_idx = 128;
     ap_int<32> a_str_idx = 0;
     ap_int<32> b_str_idx = 0;
+    
+    
+    
     while(((a_idx > 0) || (b_idx > 0))) {
       #pragma HLS loop_tripcount max=151 min=0
       ap_int<32> r = (b_idx * 129);
-      if ((ptr[(r + a_idx)] == ALIGN)) {
+      if ((ptr[b_idx][a_idx] == ALIGN)) {
         alignedA[a_str_idx] = SEQA[(a_idx - 1)];
         alignedB[b_str_idx] = SEQB[(b_idx - 1)];
         a_idx = (a_idx - 1);
         b_idx = (b_idx - 1);
       } else {
-        if ((ptr[(r + a_idx)] == SKIPB)) {
+        if ((ptr[b_idx][a_idx] == SKIPB)) {
           alignedA[a_str_idx] = SEQA[(a_idx - 1)];
           alignedB[b_str_idx] = DASH;
           a_idx = (a_idx - 1);
