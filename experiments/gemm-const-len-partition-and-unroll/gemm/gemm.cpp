d1b53b6732a3fbfd989e90c1a47bd79dc401029b
Rachit Nigam <rachit.nigam12@gmail.com>
2019-11-16 11:05:48 -0500

    2d

--- experiments/gemm-const-len-partition-and-unroll/gemm/gemm.cpp
+++ experiments/gemm-const-len-partition-and-unroll/gemm/gemm.cpp
@@ -11,20 +11,22 @@ extern "C" {
     #pragma HLS INTERFACE s_axilite port = return bundle = control
 
     // Local partitioned buffers
-    TYPE m1_local[N];
-    #pragma HLS ARRAY_PARTITION variable = m1_local cyclic factor = ::PARTITION:: dim = 1
+    TYPE m1_local[ROW_SIZE][COL_SIZE];
+    #pragma HLS ARRAY_PARTITION variable = m1_local cyclic factor = ::PARTITION:: dim = 2
     #pragma HLS resource variable=m1_local core = RAM_1P_BRAM
-    TYPE m2_local[N];
+    TYPE m2_local[ROW_SIZE][COL_SIZE];
     #pragma HLS ARRAY_PARTITION variable = m2_local cyclic factor = ::PARTITION:: dim = 1
     #pragma HLS resource variable=m2_local core = RAM_1P_BRAM
-    TYPE prod_local[N];
+    TYPE prod_local[ROW_SIZE][COL_SIZE];
     #pragma HLS ARRAY_PARTITION variable = prod_local cyclic factor = ::PARTITION:: dim = 1
     #pragma HLS resource variable=prod_local core = RAM_1P_BRAM
 
     // Copy data to local buffers
-    for(int l = 0; l < N; l++) {
-      m1_local[l] = m1[l];
-      m2_local[l] = m2[l];
+    for (int r = 0; r < ROW_SIZE; r++) {
+      for (int c = 0; c < COL_SIZE; c++) {
+        m1_local[r][c] = m1[r * COL_SIZE + c];
+        m2_local[r][c] = m2[r * COL_SIZE + c];
+      }
     }
 
     // Compute GeMM
@@ -34,15 +36,17 @@ extern "C" {
         sum = 0;
         for(int k=0; k<ROW_SIZE; k++) {
           #pragma HLS unroll factor = ::PARTITION::
-          sum += m1_local[i * COL_SIZE + k] * m2_local[k * COL_SIZE + j];
+          sum += m1_local[i][k] * m2_local[k][j];
         }
-        prod_local[i * COL_SIZE + j]  = sum;
+        prod_local[i][j]  = sum;
       }
     }
 
     // Write back to output buffer
-    for(int l = 0; l < N; l++) {
-      prod[l] = prod_local[l];
+    for (int r = 0; r < ROW_SIZE; r++) {
+      for (int c = 0; c < COL_SIZE; c++) {
+        prod[r * COL_SIZE + c] = prod_local[r][c];
+      }
     }
   }
 }
