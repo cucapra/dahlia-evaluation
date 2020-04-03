54b633ec7a1b3e915c51cf5ebe1d56441e71b27d
Rachit Nigam <rachit.nigam12@gmail.com>
2020-04-03 18:18:52 -0400

    plotting style

--- benchmarking-helpers/benchmarking/plotting/plots.py
+++ benchmarking-helpers/benchmarking/plotting/plots.py
@@ -1,21 +1,21 @@
-import pandas as pd
 import seaborn as sns
 import matplotlib
 from matplotlib import pyplot as plt
 
-def make_absolute_plots(
-        df,
-        x_key,
-        y_keys,
-        group_by,
-        x_label,
-        fig_prefix,
-        dpi = 100,
-        fig_dir = "./",
-        group_labels = None,
-        group_markers = None,
-        legend = None,
-        group_runtime_supress = None):
+
+def make_absolute_plots(df,
+                        x_key,
+                        y_keys,
+                        group_by,
+                        x_label,
+                        fig_prefix,
+                        x_supress=None,
+                        dpi=100,
+                        fig_dir="./",
+                        group_labels=None,
+                        group_markers=None,
+                        legend=None,
+                        group_runtime_supress=None):
     """Make and save graphs plotting each value in `y_keys` against `x_key`.
     Plotting each set of points separately by first running group_by on each
     row.
@@ -31,25 +31,40 @@ def make_absolute_plots(
     pal = sns.color_palette('colorblind', len(groups))
     sns.set_context('paper')
 
-    for key in y_keys:
+    for y_idx, key in enumerate(y_keys):
         fig = plt.figure()
 
         for idx, group in groups:
-            if key == 'runtime_avg' and group_runtime_supress and group_runtime_supress[idx]:
+            if key == 'runtime_avg' and \
+               group_runtime_supress and group_runtime_supress[
+                    idx]:
                 continue
-            plt.scatter(x=x_key, y=key, data=group, color=pal[idx],
+            plt.scatter(x=x_key,
+                        y=key,
+                        data=group,
+                        color=pal[idx],
                         label=group_labels[idx] if group_labels else None,
                         marker=group_markers[idx] if group_markers else None,
-                        s=100, rasterized=True)
-            plt.vlines(x=x_key, ymax=key, ymin=df[key].min(), data=group, colors=pal[idx], rasterized=True)
+                        s=100,
+                        rasterized=True)
+            plt.vlines(x=x_key,
+                       ymax=key,
+                       ymin=df[key].min(),
+                       data=group,
+                       colors=pal[idx],
+                       rasterized=True)
 
         if key == 'runtime_avg':
             y_label = 'Runtime (ms)'
         else:
-            y_label = key.replace('_', ' ').capitalize()
+            labels = key.split('_')
+            labels[0] = labels[0].upper() + "s"
+            y_label = " ".join(labels)
 
         plt.ylabel(y_label, fontsize=18)
-        plt.xlabel(x_label, fontsize=18)
+        if x_supress is None or not x_supress[y_idx]:
+            print('add xlabel')
+            plt.xlabel(x_label, fontsize=18)
         plt.xticks(fontsize=16)
         plt.yticks(fontsize=16)
 
@@ -61,22 +76,27 @@ def make_absolute_plots(
             plt.legend(prop={'size': 16})
 
         fig.tight_layout()
-        fig.savefig(fig_dir + '{}-{}.pdf'.format(fig_prefix, key.replace('_', '-')), dpi=dpi)
+        fig.savefig(fig_dir +
+                    '{}-{}.pdf'.format(fig_prefix, key.replace('_', '-')),
+                    dpi=dpi)
 
         figs.append(fig)
 
     return figs
 
-def make_qual_plot(
-    df,
-    x_data,
-    y_data,
-    group_by,
-    fig_prefix,
-    legend,
-    dpi=100,
-    fig_dir="./",
-    scale_x = { "scale_fun": lambda x: x, "scale_label": "" }):
+
+def make_qual_plot(df,
+                   x_data,
+                   y_data,
+                   group_by,
+                   fig_prefix,
+                   legend,
+                   dpi=100,
+                   fig_dir="./",
+                   scale_x={
+                       "scale_fun": lambda x: x,
+                       "scale_label": ""
+                   }):
     """
     x_data and y_data should be an objects with the key and the label names.
     """
@@ -84,7 +104,7 @@ def make_qual_plot(
     groups = df.groupby(group_by, axis=0)
     # Mapping from group_name to color
     colors = {}
-    cur = 0;
+    cur = 0
     for idx, _ in groups:
         colors[idx] = cur
         cur += 1
@@ -103,7 +123,8 @@ def make_qual_plot(
                         data=group,
                         color=pal[colors[idx]],
                         label=idx,
-                        s=100, rasterized=True)
+                        s=100,
+                        rasterized=True)
 
     # Format Y-axis to have commas in the numbers
     ax.get_yaxis().set_major_formatter(
@@ -111,7 +132,8 @@ def make_qual_plot(
 
     # Scale the X-axis as needed.
     ax.get_xaxis().set_major_formatter(
-        matplotlib.ticker.FuncFormatter(lambda x, p: format(scale_x["scale_fun"](x), ',')))
+        matplotlib.ticker.FuncFormatter(
+            lambda x, p: format(scale_x["scale_fun"](x), ',')))
 
     plt.ylabel(y_data["label"])
 
@@ -120,15 +142,24 @@ def make_qual_plot(
 
     # Generate label for X-axis
     suf = scale_x["scale_label"]
-    plt.xlabel(
-        x_data["label"] + (" (" + suf + ")" if suf != "" else ""))
+    plt.xlabel(x_data["label"] + (" (" + suf + ")" if suf != "" else ""))
 
     fig.tight_layout()
-    fig.savefig(fig_dir + '{}-{}.pdf'.format(fig_prefix, y_key.replace('_', '-')), dpi=dpi)
+    fig.savefig(fig_dir +
+                '{}-{}.pdf'.format(fig_prefix, y_key.replace('_', '-')),
+                dpi=dpi)
 
-        # figs.append(fig)
+    # figs.append(fig)
 
-def make_sec2_plot(df, x_key, x_label, fig_prefix, factor=16, dpi=100, fig_dir="./", legend = None):
+
+def make_sec2_plot(df,
+                   x_key,
+                   x_label,
+                   fig_prefix,
+                   factor=16,
+                   dpi=100,
+                   fig_dir="./",
+                   legend=None):
     y_keys = [
         'runtime_avg',
         'lut_used',
@@ -140,16 +171,19 @@ def make_sec2_plot(df, x_key, x_label, fig_prefix, factor=16, dpi=100, fig_dir="
             return 2
         return int(factor % df.iloc[idx][x_key] == 0)
 
-    make_absolute_plots(
-        df,
-        x_key,
-        y_keys,
-        dpi=dpi,
-        fig_dir = fig_dir,
-        group_by = group,
-        group_labels = ['Unpredictable points', 'Predictable points', 'Incorrect hardware'],
-        group_markers = ['o', 'v', 'x'],
-        group_runtime_supress = [ False, False, True ],
-        x_label = x_label,
-        fig_prefix = fig_prefix,
-        legend = legend)
+    make_absolute_plots(df,
+                        x_key,
+                        y_keys,
+                        dpi=dpi,
+                        fig_dir=fig_dir,
+                        group_by=group,
+                        group_labels=[
+                            'Unpredictable points', 'Predictable points',
+                            'Incorrect hardware'
+                        ],
+                        group_markers=['o', 'v', 'x'],
+                        group_runtime_supress=[False, False, True],
+                        x_supress=[False, True],
+                        x_label=x_label,
+                        fig_prefix=fig_prefix,
+                        legend=legend)
